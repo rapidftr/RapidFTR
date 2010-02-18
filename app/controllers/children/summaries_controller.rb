43856b3a6d8be3965d3c409d3d9e60870d0eaa84
Zubair <zkhan@ihytti.corporate.thoughtworks.com>
2010-02-18 19:44:20 +0000

    First cut of summary controller

--- app/controllers/children/summaries_controller.rb
+++ app/controllers/children/summaries_controller.rb
@@ -4,7 +4,8 @@ class Children::SummariesController < ApplicationController
   end
 
   def create
-    SearchRequest.new(params[:search_request])
+    search_request = SearchRequest.new(ApplicationController.current_user.user_name, params[:search_request])
+    search_request.save
     redirect_to children_summary_path
   end
 
--- spec/controllers/children/summaries_controller_spec.rb
+++ spec/controllers/children/summaries_controller_spec.rb
@@ -3,18 +3,37 @@ require 'spec_helper'
 describe Children::SummariesController, "POST create" do
 
   before(:each) do
-    @search_request = {"name"=> "Willis"}
+    @user = User.new
+    @user.user_name = "ausername"
+    ApplicationController.stub(:current_user).and_return(@user)
+    @search_request_params = {"name"=> "Willis"}
   end
 
-  after(:each) do
-    post :create, :search_request => @search_request
+  def post_request
+    post :create, :search_request => @search_request_params
   end
 
   it "creates a new seach request for the user" do
-    SearchRequest.should_receive(:new).with(@search_request)
+    SearchRequest.should_receive(:new).with(@search_request_params)
+    post_request
   end
   
-  it "saves the search request for the user"
+  it "saves the search request for the user"  do
+    search_request = stub('SearchRequest')
 
-  it "redirects to the summaries view"
+    SearchRequest.stub(:new).with(@user.user_name, @search_request_params).and_return(search_request)
+    search_request.should_receive(:save)
+    post_request
+
+  end
+
+  it "redirects to the summaries view" do
+    search_result = stub('search_result')
+    SearchRequest.stub(:new).with(anything, anything).and_return(search_result)
+    search_result.should_receive(:save)
+    
+    post_request
+
+    response.should redirect_to("/children/summary")
+  end
 end
