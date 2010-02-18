eb78f735dc7797ffae419dd4dee5e4ead988f6b4
Zubair <zkhan@ihytti.corporate.thoughtworks.com>
2010-02-18 19:44:20 +0000

    First cut of summary controller

--- app/models/search_request.rb
+++ app/models/search_request.rb
@@ -1,4 +1,10 @@
 class SearchRequest < CouchRestRails::Document
   use_database :search_request
-  
+
+  property :user_name
+
+  def initialize(for_user_name, *args)
+    super args
+    @user_name = for_user_name
+  end
 end
