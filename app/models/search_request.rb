29beab84ad6f776ccc74f3f168c29d666f0e3751
Zubair <zkhan@Zubairs-MacBook-Pro.local>
2010-02-22 12:49:48 +0000

    Made search request per user using factory method.

--- app/models/search_request.rb
+++ app/models/search_request.rb
@@ -20,10 +20,22 @@ class SearchRequest < CouchRestRails::Document
     end
   end
 
-  property :user_name
+  def user_name=(value)
+    self['_id'] = value
+  end
 
-  def initialize(for_user_name, *args)
-    super args
-    @user_name = for_user_name
+  def _id
+    return @user_name
+  end
+
+  def self.create_search(user_name, *fields)
+    if (SearchRequest.get(user_name))
+      search = SearchRequest.get(user_name)
+      search.merge! fields[0]
+    else
+      search = SearchRequest.new(*fields)
+      search.user_name = user_name
+      return search
+    end
   end
 end
