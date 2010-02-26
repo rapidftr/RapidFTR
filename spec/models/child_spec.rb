93e90fb364ac5e1843423cbb4896d4ec13600788
cgeorge <admin@thought-workers-macbook-pro-4.local>
2010-02-26 17:27:49 -0500

    do not record any history if a save happens without any actual changes

--- spec/models/child_spec.rb
+++ spec/models/child_spec.rb
@@ -74,89 +74,99 @@ describe Child do
 
   describe "history log" do
     it "should not update history on initial creation of child document" do
-      @child = Child.new('last_known_location' => 'New York')
-      @child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
-      @child.save
-
-      @child['histories'].should be_empty
+      child = Child.new('last_known_location' => 'New York')
+      child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
+      child.save!
+    
+      child['histories'].should be_empty
     end
-
+    
     it "should update history with 'from' value on last_known_location update" do
-      @child = Child.new('last_known_location' => 'New York')
-      @child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
-      @child.save!
-
-      @child['last_known_location'] = 'Philadelphia'
-      @child.save!
-
-      changes = @child['histories'].first['changes']
+      child = Child.new('last_known_location' => 'New York')
+      child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
+      child.save!
+      
+      child['last_known_location'] = 'Philadelphia'
+      child.save!
+    
+      changes = child['histories'].first['changes']
       changes['last_known_location']['from'].should == 'New York'
     end
-
+    
     it "should update history with 'to' value on last_known_location update" do
-      @child = Child.new('last_known_location' => 'New York')
-      @child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
-      @child.save!
-
-      @child['last_known_location'] = 'Philadelphia'
-      @child.save!
-
-      changes = @child['histories'].first['changes']
+      child = Child.new('last_known_location' => 'New York')
+      child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
+      child.save!
+      
+      child['last_known_location'] = 'Philadelphia'
+      child.save!
+    
+      changes = child['histories'].first['changes']
       changes['last_known_location']['to'].should == 'Philadelphia'
     end
-
+    
     it "should update history with 'from' value on age update" do
-      @child = Child.new('age' => '8', 'last_known_location' => 'New York')
-      @child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
-      @child.save!
-
-      @child['age'] = '6'
-      @child.save!
-
-      changes = @child['histories'].first['changes']
+      child = Child.new('age' => '8', 'last_known_location' => 'New York')
+      child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
+      child.save!
+      
+      child['age'] = '6'
+      child.save!
+    
+      changes = child['histories'].first['changes']
       changes['age']['from'].should == '8'
     end
-
+    
     it "should update history with 'to' value on age update" do
-      @child = Child.new('age' => '8', 'last_known_location' => 'New York')
-      @child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
-      @child.save!
-
-      @child['age'] = '6'
-      @child.save!
-
-      changes = @child['histories'].first['changes']
+      child = Child.new('age' => '8', 'last_known_location' => 'New York')
+      child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
+      child.save!
+      
+      child['age'] = '6'
+      child.save!
+    
+      changes = child['histories'].first['changes']
       changes['age']['to'].should == '6'
     end
-
+    
     it "should update history with a combined history record when multiple fields are updated" do
-      @child = Child.new('age' => '8', 'last_known_location' => 'New York')
-      @child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
-      @child.save!
-
-      @child['age'] = '6'
-      @child['last_known_location'] = 'Philadelphia'
-      @child.save!
-
-      @child['histories'].size.should == 1
-      changes = @child['histories'].first['changes']
+      child = Child.new('age' => '8', 'last_known_location' => 'New York')
+      child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
+      child.save!
+      
+      child['age'] = '6'
+      child['last_known_location'] = 'Philadelphia'
+      child.save!
+      
+      child['histories'].size.should == 1
+      changes = child['histories'].first['changes']
       changes['age']['from'].should == '8'
       changes['age']['to'].should == '6'
       changes['last_known_location']['from'].should == 'New York'
       changes['last_known_location']['to'].should == 'Philadelphia'
+    end
+    
+    it "should not record anything in the history if a save occured with no changes" do
+      child = Child.new('last_known_location' => 'New York')
+      child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
+      child.save!
+      loaded_child = Child.get(child.id)
+      loaded_child.save!
+      
+      loaded_child['histories'].should be_empty
     end
 
     it "should update history with the correct date and time when a change has occurred" do
       current_datetime = stub(:strftime => "3/15/2010 04:45")
       Time.stub!(:now).and_return current_datetime
-      @child = Child.new('last_known_location' => 'New York')
-      @child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
-      @child.save!
-
-      @child['last_known_location'] = 'Philadelphia'
-      @child.save!
-
-      @child['histories'].first['datetime'].should == "3/15/2010 04:45"
+      child = Child.new('last_known_location' => 'New York')
+      child.instance_variable_set(:'@file_name', 'some_file.jpg') # to pass photo validation
+      child.save!
+      
+      child['last_known_location'] = 'Philadelphia'
+      child.save!
+    
+      child['histories'].first['datetime'].should == "3/15/2010 04:45"
     end
   end
 end
