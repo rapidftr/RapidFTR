f9683000ad7e5c033af2eccb189c05c45057b54a
Babitha <brakesh@thoughtworkers-macbook-pro-10.local>
2010-03-15 14:28:42 +0000

    Alex:When adding a new child,forms are now available under corresponding tabs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var RapidFTR = {};
+
+// START: Tabs
+RapidFTR.tabControl = function(){
+	$(".tab").hide(); //Hide all content
+	$(".tab-handles li:first").addClass("current").show(); //Activate first tab
+	$(".tab:first").show(); //Show first tab content
+
+	//On Click Event
+	$(".tab-handles a").click(function() {
+
+		$(".tab-handles li").removeClass("current"); //Remove any "active" class
+		$(".tab").hide(); //Hide all tab content
+
+		var activeTab = $(this).attr("href"); //Find the href attribute value to identify the active tab + content
+
+		$(this).parent().addClass("current"); //Add "active" class to selected tab
+		$(activeTab).show(); //Fade in the active ID content
+		return false;
+	});
+
+};
