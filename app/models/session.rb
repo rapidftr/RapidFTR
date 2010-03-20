695bacc2fd54f877bcb7b1ffdafae163c7923ed9
John D. Hume <duelin.markers@gmail.com>
2010-03-20 19:15:13 -0400

    Remove unused authentication method from Session.

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,18 +3,6 @@ class Session < CouchRestRails::Document
 
   property :user_name
 
-  def autheniticate_user
-    user = User.find_by_user_name(@user_name)
-
-    authenticated = !user.nil? && user.autheticate(@password)
-
-    if not authenticated
-      errors.add(:base, "Invalid credentials. Please try again!")
-    end
-
-    authenticated
-  end
-
   def self.get_from_cookies(cookies)
     session_id = cookies[:session_id]
     self.get(session_id)
