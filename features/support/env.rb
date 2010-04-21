ffb9edf9addd9eba42227e61f12bd1cb09707eff
Tom Elkin <telkin@thoughtworks.com>
2010-04-21 14:52:55 +0100

    Remove dodgy 'require' from env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,6 @@
 
 ENV["RAILS_ENV"] ||= "cucumber"
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
-require File.expand_path(File.dirname(__FILE__) + '/../../db/resetcouchdb')
 
 require 'cucumber/formatter/unicode' # Remove this line if you don't want Cucumber Unicode support
 require 'cucumber/rails/rspec'
