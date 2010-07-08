81aafee7253b470045d0bd85fe1a5b8aebd8303a
jennifersmith <jennifer.e.m.smith@gmail.com>
2010-07-08 21:27:24 +0100

    Changed back to hidden fields and jigged around view tests to remove/make less brittle

--- app/helpers/fields_helper.rb
+++ app/helpers/fields_helper.rb
@@ -2,7 +2,7 @@ module FieldsHelper
 	def option_fields_for form, suggested_field
 		return "" unless suggested_field.field.option_strings
 		suggested_field.field.option_strings.collect do |option_string|
-	  		form.text_field "option_strings][", { :id => "option_strings", :value => option_string }
+	  		form.hidden_field "option_strings][", { :id => "option_string_" + option_string, :value => option_string }
 		end
 	end
 	def display_options field
