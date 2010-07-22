90f1be02acdc4c7a832671f5e36153d91a07bd8b
Sharanya <sharanya@sharanya-laptop.(none)>
2010-07-22 18:56:20 +0530

    #161 - Modified PublishedFormSection to return json response - for Blackberry client

--- app/controllers/publish_form_section_controller.rb
+++ app/controllers/publish_form_section_controller.rb
@@ -2,6 +2,10 @@ class PublishFormSectionController < ApplicationController
 
   def form_sections
     enabled_form_sections = FormSection.all_by_order.reject{|fs| !fs.enabled}
-    render :inline => enabled_form_sections.to_json
+    json_content = enabled_form_sections.to_json
+    respond_to do |format|
+      format.html {render :inline => json_content }
+      format.json { render :json => json_content }
+    end
   end
 end
