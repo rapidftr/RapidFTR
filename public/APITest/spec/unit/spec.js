5f896bc3141cdcd88e15c2f4394f724f891c02e9
Tom Elkin <telkin@thoughtworks.com>
2010-08-14 18:12:04 +0100

    Return correct JSON response when logging in with invalid password

--- public/APITest/spec/unit/spec.js
+++ public/APITest/spec/unit/spec.js
@@ -17,41 +17,66 @@ describe 'API Test'
 
     urlPrefix = "http://" + window.location.host;
 
-	var token
-	var childid
-	var response = {}
-	var parameters = {}
+	var token;
+	var childid;
+	var response = {};
+	var parameters = {};
+    var request = {};
 
-	$.ajaxSetup({async:false})
+	$.ajaxSetup({async:false});
 	
 	before_each
-		response = {}
-		parameters = {}
+		response = {};
+		parameters = {};
+        request = {};
 	end
 	
-	it 'should return a autentication token'
-	
+		it 'should return a autentication token'
+
 		parameters = {user_name: "rapidftr", password: "rapidftr"}
-		
+
 		$.ajax({
 			url: urlPrefix + "/sessions",
 			dataType: 'json',
 			data: parameters,
 			type: 'POST',
 			cache: false,
-			success: function(data)
+			success: function(data, status)
 			{
 				response = data
 		    },
-			complete: function(request, textStatus) 
+			complete: function(xmlHttpRequest)
 			{
-				token = response.session.token
+                request = xmlHttpRequest;
+                token = response.session.token
+
 			}
-		})
-		
+		});
+
+        request.status.should.eql 201
 		response.should.not.be_empty
 		token.should.not.be_empty
-		
+
+	end
+
+    it 'should return 401 not authorized when authenticating with invalid password'
+
+		parameters = {user_name: "rapidftr", password: "wrongpassword"};
+
+		$.ajax({
+			url: urlPrefix + "/sessions",
+			dataType: 'json',
+			data: parameters,
+			type: 'POST',
+			cache: false,
+			complete: function(xmlHttpRequest)
+			{
+                request = xmlHttpRequest;
+
+			}
+		});
+
+        request.status.should.eql 401
 	end
 	
 	it 'should create a new child record'
@@ -120,17 +145,36 @@ describe 'API Test'
 		  	success: function(data)
 			{
 				//alert(data.toSource());
-				
+
 				response = data
 		    }
 		})
-		
+
 		response.should.not.be_empty
 		response.should.have_property "name"
 		response.should.have_property "last_known_location"
-		
+
 	end
-	
+
+    it 'should return 401 not authorized when making a request with invalid token'
+
+        $.ajax({
+            url: urlPrefix + "/children/" + childid,
+              dataType: 'json',
+            cache: false,
+            beforeSend: function(request)
+            {
+                request.setRequestHeader('Authorization', "RFTR_Token " + "Invalid token")
+            },
+            complete: function(xmlHttpRequest)
+            {
+                request = xmlHttpRequest;
+            }
+        })
+
+        request.status.should.eql 401
+    end
+
 	it 'should update record of child with specified id'
 
 		$.ajax({
