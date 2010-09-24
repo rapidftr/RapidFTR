38e3f7ecde2a7704b3a2af20c77ad267f8e7a717
Aravind <arav.psgtech@gmail.com>
2010-09-24 16:09:19 +0530

    [197] - Disabled forms should not be shown in create/edit/show child page - [Aravind]

--- spec/views/child/show.html.erb_spec.rb
+++ spec/views/child/show.html.erb_spec.rb
@@ -11,7 +11,7 @@ describe "children/show.html.erb" do
 
     it "renders all fields found on the FormSection" do
 
-      form_section = FormSection.new :unique_id => "section_name"
+      form_section = FormSection.new :unique_id => "section_name", :enabled => "true"
       form_section.add_field Field.new_text_field("age")
       form_section.add_field Field.new_radio_button("gender", ["male", "female"])
       form_section.add_field Field.new_select_box("date_of_separation", ["1-2 weeks ago", "More than"])
@@ -40,6 +40,32 @@ describe "children/show.html.erb" do
         fields[0].should contain("27")
         fields[1].should contain("male")
         fields[2].should contain("1-2 weeks ago")
+      end
+    end
+
+    it "does not render fields found on a disabled FormSection" do
+
+      form_section = FormSection.new :unique_id => "section_name", :enabled => "false"
+      form_section.add_field Field.new_text_field("age")
+      form_section.add_field Field.new_radio_button("gender", ["male", "female"])
+
+      child = Child.new :age => "27", :gender => "male", :unique_identifier => "georgelon12345", :_id => "id12345"
+
+      assigns[:form_sections] = [form_section]
+      assigns[:child] = child
+
+      render
+
+      response.should have_selector("dt:first") do |dt|
+        dt.should contain("Unique Id")
+      end
+      response.should have_selector("dd:first") do |dd|
+        dd.should contain("georgelon12345")
+      end
+
+      response.should_not have_selector("dl.section_name dt") do |fields|
+        fields[0].should_not contain("Age")
+        fields[1].should_not contain("Gender")
       end
     end
 
