2205c6676c112ffb135ce38a2823806714947155
Tom Elkin <telkin@thoughtworks.com>
2010-09-24 13:04:31 +0100

    [165] Should *always* do a 'rake ci:build' before check in... :) Fixed test

--- lib/rapidftr_default_db_setup.rb
+++ lib/rapidftr_default_db_setup.rb
@@ -21,33 +21,34 @@ module RapidFTR
       FormSection.create!("name" =>"Basic details", "enabled"=>true, :description => "Basic information about a child", :order=> 1, :unique_id=>"basic_details", :editable => false, :fields => basic_details_fields)
 
       family_details_fields = [
-              Field.new("name" => "father_name", "type" => "text_field"),
+              Field.new("name" => "fathers_name", "type" => "text_field"),
               Field.new("name" => "is_father_alive", "type" => "radio_button", "option_strings" => ["Yes", "No", "Unknown"]),
-              Field.new("name" => "reunite_with_father", "type" => "check_box"),
-              Field.new("name" => "mother_name", "type" => "text_field"),
+              Field.new("name" => "reunite_with_father", "type" => "select_box", "option_strings" => ["Yes", "No"]),
+              Field.new("name" => "mothers_name", "type" => "text_field"),
               Field.new("name" => "is_mother_alive", "type" => "radio_button", "option_strings" => ["Yes", "No", "Unknown"]),
-              Field.new("name" => "reunite_with_mother", "type" => "check_box"),
+              Field.new("name" => "reunite_with_mother", "type" => "select_box", "option_strings" => ["Yes", "No"]),
               Field.new("name" => "relative_1_name", "type" => "text_field"),
               Field.new("name" => "relative_1_relationship", "type" => "text_field"),
               Field.new("name" => "is_relative_1_alive", "type" => "radio_button", "option_strings" => ["Yes", "No", "Unknown"]),
-              Field.new("name" => "reunite_with_relative_1", "type" => "check_box"),
+              Field.new("name" => "reunite_with_relative_1", "type" => "select_box", "option_strings" => ["Yes", "No"]),
               Field.new("name" => "relative_2_name", "type" => "text_field"),
               Field.new("name" => "relative_2_relationship", "type" => "text_field"),
               Field.new("name" => "is_relative_2_alive", "type" => "radio_button", "option_strings" => ["Yes", "No", "Unknown"]),
-              Field.new("name" => "reunite_with_relative_2", "type" => "check_box"),
+              Field.new("name" => "reunite_with_relative_2", "type" => "select_box", "option_strings" => ["Yes", "No"]),
               Field.new("name" => "relative_3_name", "type" => "text_field"),
               Field.new("name" => "relative_3_relationship", "type" => "text_field"),
               Field.new("name" => "is_relative_3_alive", "type" => "radio_button", "option_strings" => ["Yes", "No", "Unknown"]),
-              Field.new("name" => "reunite_with_relative_3", "type" => "check_box"),
+              Field.new("name" => "reunite_with_relative_3", "type" => "select_box", "option_strings" => ["Yes", "No"]),
               Field.new("name" => "relative_4_name", "type" => "text_field"),
               Field.new("name" => "relative_4_relationship", "type" => "text_field"),
               Field.new("name" => "is_relative_4_alive", "type" => "radio_button", "option_strings" => ["Yes", "No", "Unknown"]),
-              Field.new("name" => "reunite_with_relative_4", "type" => "check_box"),
-              Field.new("name" => "married", "type" => "check_box"),
+              Field.new("name" => "reunite_with_relative_4", "type" => "select_box", "option_strings" => ["Yes", "No"]),
+              Field.new("name" => "married", "type" => "select_box", "option_strings" => ["Yes", "No"]),
               Field.new("name" => "spouse_or_partner_name", "type" => "text_field"),
               Field.new("name" => "no_of_children", "type" => "text_field"),
       ]
-              FormSection.create!("name" =>"Family details", "enabled"=>true, :description =>"Information about a child's known family", :order=> 2, :unique_id=>"family_details", :fields => family_details_fields)
+
+      FormSection.create!("name" =>"Family details", "enabled"=>true, :description =>"Information about a child's known family", :order=> 2, :unique_id=>"family_details", :fields => family_details_fields)
 
 
       caregiver_details_fields = [
