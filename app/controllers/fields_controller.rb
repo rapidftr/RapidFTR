395ecdb06889dc29a7a058de04b44a5d65a441b9
Andrew Nesbitt <andrewnez@gmail.com>
2010-09-25 17:01:45 +0100

    Fixed deprecation warning

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -40,7 +40,7 @@ class FieldsController < ApplicationController
   end
 
   def split_option_strings properties
-    if properties[:option_strings] && properties[:type] == "select_box" && properties[:option_strings].type != Array
+    if properties[:option_strings] && properties[:type] == "select_box" && properties[:option_strings].class != Array
       properties[:option_strings]=properties[:option_strings].split("\r\n")
     end
   end
