2c3869afce4778f3862fd6c50c89aadb9759b4dd
Carlos Villela <cv@lixo.org>
2010-10-04 15:30:48 -0300

    removed unnecessary tests

--- test/functional/fields_controller_test.rb
+++
@@ -1,6 +0,0 @@
-require 'test_helper'
-
-class FieldsControllerTest < ActionController::TestCase
-  test "the truth" do
-  end
-end
--- test/unit/helpers/fields_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class FieldsHelperTest < ActionView::TestCase
-end
