654a00047d7804ac979ac7f6baed7cd5cb031470
Carlos Villela <cv@lixo.org>
2010-10-21 20:15:28 -0200

    Convert tabs to spaces and back again: now soft tabbing at 2 in application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,60 +2,60 @@ var RapidFTR = {};
 
 // START: Tabs
 RapidFTR.tabControl = function() {
-    $(".tab").hide(); //Hide all content
-    $(".tab-handles li:first").addClass("current").show(); //Activate first tab
-    $(".tab:first").show(); //Show first tab content
+  $(".tab").hide(); //Hide all content
+  $(".tab-handles li:first").addClass("current").show(); //Activate first tab
+  $(".tab:first").show(); //Show first tab content
 
-    //On Click Event
-    $(".tab-handles a").click(function() {
+  //On Click Event
+  $(".tab-handles a").click(function() {
 
-        $(".tab-handles li").removeClass("current"); //Remove any "active" class
-        $(".tab").hide(); //Hide all tab content
+    $(".tab-handles li").removeClass("current"); //Remove any "active" class
+    $(".tab").hide(); //Hide all tab content
 
-        var activeTab = $(this).attr("href"); //Find the href attribute value to identify the active tab + content
+    var activeTab = $(this).attr("href"); //Find the href attribute value to identify the active tab + content
 
-        $(this).parent().addClass("current"); //Add "active" class to selected tab
-        $(activeTab).show(); //Fade in the active ID content
-        return false;
-    });
+    $(this).parent().addClass("current"); //Add "active" class to selected tab
+    $(activeTab).show(); //Fade in the active ID content
+    return false;
+  });
 
-    // submitting forms with links
-    $(".submit-form").click(function()
-    {
-        var formToSubmit = $(this).attr("href");
-        $(formToSubmit).submit();
-        return false;
-    });
+  // submitting forms with links
+  $(".submit-form").click(function()
+  {
+    var formToSubmit = $(this).attr("href");
+    $(formToSubmit).submit();
+    return false;
+  });
 
-    $(document.getElementById("enable_form")).click(function()
-    {
+  $(document.getElementById("enable_form")).click(function()
+  {
 
-        var form = document.getElementById("enable_or_disable_form_section");
-        form.action = 'form_section/enable';
-        form.submit();
-        return true;
-    });
-
-
-    $(document.getElementById("disable_form")).click(function()
-    {
-
-        var form = document.getElementById("enable_or_disable_form_section");
-        form.action = 'form_section/disable';
-        form.submit();
-        return true;
-    });
+    var form = document.getElementById("enable_or_disable_form_section");
+    form.action = 'form_section/enable';
+    form.submit();
+    return true;
+  });
 
 
-    //hiding field direction buttons (first up button and second down)
-    $("#formFields .up-link:first").hide();
-    $("#formFields .down-link:last").hide();
+  $(document.getElementById("disable_form")).click(function()
+  {
+
+    var form = document.getElementById("enable_or_disable_form_section");
+    form.action = 'form_section/disable';
+    form.submit();
+    return true;
+  });
+
+
+  //hiding field direction buttons (first up button and second down)
+  $("#formFields .up-link:first").hide();
+  $("#formFields .down-link:last").hide();
 };
 
 RapidFTR.followTextFieldControl = function(selector, followSelector, transformFunction){
-    $(selector).keyup(function(){
-		var val = $(this).val();	
-		var transformed =  transformFunction(val);
-       	$(followSelector).val(transformed); 
-    });
+  $(selector).keyup(function(){
+    var val = $(this).val();  
+    var transformed =  transformFunction(val);
+    $(followSelector).val(transformed); 
+  });
 }
