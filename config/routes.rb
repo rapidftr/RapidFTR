b70209ed85e9695d472c6e5f4bba2e8e1303d41b
Carlos Villela <cv@lixo.org>
2010-10-24 03:04:41 -0200

    tidy up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources(
-          :children,
-          :collection => { :search => :get, :photo_pdf => :post, :advanced_search => :get } ) do |child|
+
+  map.resources :children, :collection => { :search => :get, :photo_pdf => :post, :advanced_search => :get } do |child|
     child.resource :history, :only => :show
     child.resources :attachments, :only => :show
   end
@@ -17,31 +16,23 @@ ActionController::Routing::Routes.draw do |map|
   map.admin 'admin', :controller=>"admin", :action=>"index"
   map.resources :sessions, :except => :index
 
-
   map.login 'login', :controller=>'sessions', :action =>'new'
   map.logout 'logout', :controller=>'sessions', :action =>'destroy'
-
 
   map.enable_form 'form_section/enable', :controller => 'form_section', :action => 'enable', :value => true, :conditions => {:method => :post}
   map.disable_form 'form_section/disable', :controller => 'form_section', :action => 'enable', :value => false
 
+  map.resources :formsections, :controller => 'form_section' do |form_section|
 
-  map.resources :formsections, :controller=>'form_section' do |form_section|
-
-    field_types = FieldsController::FIELD_TYPES
-    additional_field_actions = field_types.inject({}){ |h, type| h["new_#{type}"] = :get; h }
+    additional_field_actions = FieldsController::FIELD_TYPES.inject({}){|h, type| h["new_#{type}"] = :get; h }
     additional_field_actions[:new] = :get
     additional_field_actions[:edit_field] = :get
     additional_field_actions[:move_up] = :post
     additional_field_actions[:move_down] = :post
     additional_field_actions[:delete] = :post
-    form_section.resources(
-            :fields,
-            :controller => 'fields',
-            :collection => additional_field_actions )
+    
+    form_section.resources :fields, :controller => 'fields', :collection => additional_field_actions
   end
-
-
 
   map.published_form_sections '/published_form_sections', :controller => 'publish_form_section', :action => 'form_sections'
 
@@ -50,4 +41,5 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :fields
 
   map.root :controller => 'home', :action => :index
+
 end
