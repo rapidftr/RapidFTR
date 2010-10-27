1bec753d4ba6494133bc6832ba17638bebe588b2
Tom Elkin <telkin@thoughtworks.com>
2010-10-27 10:45:30 +0100

    Revert "Cesar/Magrin - #77 - Added an edit button to field in the forms section." - currently the edit button for a Field does nothing.
    This reverts commit 073a08d3445bf3c2f97f43ae3ba7300bf74784a9.

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -63,10 +63,6 @@ class FieldsController < ApplicationController
     redirect_to(formsection_fields_path(params[:formsection_id]))
   end
 
-  def edit_field
-    redirect_to(formsection_fields_path(params[:formsection_id]))
-  end
-
   FIELD_TYPES.each do |field_type|
     define_method "new_#{field_type}" do
       read_form_section()
