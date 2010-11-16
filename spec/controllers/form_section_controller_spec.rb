1b96ab81c71a3c0a83be69d547c21f44499d9eb8
cgeorge <admin@thought-workers-macbook-pro-4.local>
2010-11-15 22:45:50 -0500

    CG: #90 - missed a duplicate save_order test after the rebase, which put multiple form_sections with the same unique_id in the database

--- spec/controllers/form_section_controller_spec.rb
+++ spec/controllers/form_section_controller_spec.rb
@@ -102,16 +102,4 @@ describe FormSectionController do
       post :enable, :value => true, :sections => {"form_1" => 1, "form_2" => 1}, :controller => "form_section"
     end
   end
-  
-  describe "post save_order" do
-    it "should save the order of the forms" do
-      form_one = FormSection.create_new_custom("first form")
-      form_two = FormSection.create_new_custom("second form")
-      form_three = FormSection.create_new_custom("third form")       
-      post :save_order, :form_order => {form_one.unique_id.to_s => "3", form_two.unique_id.to_s => "1", form_three.unique_id.to_s => "2"}
-      FormSection.get(form_one.id).order.should == 3
-      FormSection.get(form_three.id).order.should == 2
-      FormSection.get(form_two.id).order.should == 1
-    end
-  end
 end
