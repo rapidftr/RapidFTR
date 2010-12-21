5243ec8c2f83e7ace242ab4eda4540b2d776c7fd
Jen Smith <jennifer@home>
2010-12-21 18:18:49 +0000

    Now made the admin form serve up a json response for mobile

--- spec/controllers/contact_information_controller_spec.rb
+++ spec/controllers/contact_information_controller_spec.rb
@@ -12,4 +12,15 @@ describe ContactInformationController do
       assigns[:contact_information].should == contact_information
     end
   end
+  describe "GET show" do
+    it "returns JSON version of the contact information when requested" do
+      @request.env["HTTP_ACCEPT"] = "application/json"
+      contact_information = {"name"=>"Bob"}
+      ContactInformation.stub!(:get_by_id).with("administrator").and_return(contact_information)
+      get :show, :id => "administrator"
+      response_as_json =  JSON.parse @response.body
+      response_as_json.should == {"name"=>"Bob"}
+    end
+  end
+  
 end
