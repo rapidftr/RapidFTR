44aa21371ce5650407f0c44e6a9af13111b49d72
Jen Smith <jennifer@home>
2010-12-26 21:08:22 +0000

    Made sure that the only create-on-get is when you go to edit the contact info as an admin.

--- app/models/contact_information.rb
+++ app/models/contact_information.rb
@@ -12,6 +12,11 @@ class ContactInformation < CouchRestRails::Document
   
   def self.get_by_id id
     result = self.all.select{|x|x.id==id}.first
+    raise ErrorResponse.not_found("Cannot find ContactInformation with id #{id}") if result.nil?
+    return result
+  end
+  def self.get_or_create id
+    result = self.all.select{|x|x.id==id}.first
     return result if !result.nil?
     new_contact_info = ContactInformation.new :id=>id
     new_contact_info.save!
