0087640106c8d30696776fc17532b6f77ee4eccb
Sandeep Joshi <sandeep@Sandeep.(none)>
2010-12-27 14:53:31 +0530

    #520 Sandeep/ Akshay:normal User should not see Admin Link : Fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,15 @@ module ApplicationHelper
     return session.user_name unless session.nil? or session.user.nil?
   end
 
+
+  def is_admin?
+    session = Session.get_from_cookies(cookies)
+    user = User.find_by_user_name session.user_name
+    user.user_type == "User" ? false : true
+
+  end
+
+
   def submit_button(name = 'Save')
     content_tag(:p, :class => 'submitButton') do
       submit_tag(name)
