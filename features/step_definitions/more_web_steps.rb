2194b23c274d615c4024f41489b795db33cfa5fa
Jen Smith <jennifer@home>
2011-01-01 13:21:57 +0000

    Fixed failing scenario as had changed audio url

--- features/step_definitions/more_web_steps.rb
+++ features/step_definitions/more_web_steps.rb
@@ -98,7 +98,7 @@ Then /^the user "([^\"]*)" should be marked as (disabled|enabled)$/ do |username
 end
 
 Then /^I should see an audio element that can play the audio file named "([^"]*)"$/ do |filename|
-  response_body.should have_selector("audio source", :src=>request.url + "/audio")
+  response_body.should have_selector("audio source", :src=>request.url + "/audio/")
 end
 
 Then /^I should not see an audio tag$/ do
