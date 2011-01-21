ea92d324f8576954d2478b02d401fabe928c9417
John D. Hume <duelin.markers@gmail.com>
2011-01-21 10:20:56 +0300

    Add hoptoad for error notification.

---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,5 @@
+# Uncomment and enter your api_key if you're using hoptoad.
+# Chef will remove this and symlink to shared/config/initializers/hoptoad.rb.
+# HoptoadNotifier.configure do |config|
+#   config.api_key = PUT YOUR API KEY STRING HERE
+# end
