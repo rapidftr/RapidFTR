37622baa6e2b85bfa6e4d0d1c9060631d4d5f94b
Jason Neylon <jason.neylon@uswitch.com>
2011-03-14 19:37:03 +0000

    Restructure the test

--- spec/integration/pdf_generator_spec.rb
+++ spec/integration/pdf_generator_spec.rb
@@ -2,16 +2,6 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe PdfGenerator do
 
-  before :all do  
-    form = FormSection.new(:name => "test_form", :order => 1 )
-    p form
-    form.save!
-  end
-  
-  after :all do
-    FormSection.all.each{ |form| form.destroy }
-  end
-
   it "should_generate_a_pdf" do
     child = Child.new_with_user_name("jdoe", {
         "name" => "Dave",
@@ -20,13 +10,25 @@ describe PdfGenerator do
     
     subject.children_info [child]
   end
+
+  describe "when a section is blank" do
+    before :all do  
+      form = FormSection.new(:name => "test_form", :order => 1 )
+      form.save!
+    end
+
+    after :all do
+      FormSection.all.each{ |form| form.destroy }
+    end
+
+    it "should not fail" do
+      child = Child.new_with_user_name("jdoe", {
+          "name" => "Dave",
+          "age" => "28",
+          "last_known_location" => "London"})
+
+      subject.children_info [child]
+     end
+  end
   
-  it "should not fail when a section is blank" do
-    child = Child.new_with_user_name("jdoe", {
-        "name" => "Dave",
-        "age" => "28",
-        "last_known_location" => "London"})
-    
-    subject.children_info [child]
-   end
 end
