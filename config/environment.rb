5c50180bf184b77df1600acab879c72e1895449d
Michael Schubert <michael@schubert.cx>
2011-03-15 00:29:29 +0000

    deprecation fix: config.load_path -> config.autoload_paths

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  config.load_paths += %W( #{RAILS_ROOT}/lib/extensions )
+  config.autoload_paths += %W( #{RAILS_ROOT}/lib/extensions )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
