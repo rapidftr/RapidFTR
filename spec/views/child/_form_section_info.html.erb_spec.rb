09113b7fdb4c7cee7efe9d4721d5a24aa09420c4
dahlia <dlbock@gmail.com>
2011-03-22 22:14:26 -0400

    [#633] Fixing some tests.

--- spec/views/child/_form_section_info.html.erb_spec.rb
+++ spec/views/child/_form_section_info.html.erb_spec.rb
@@ -1,44 +1,45 @@
 require 'spec_helper'
 
 describe "children/_form_section_info.html.erb" do
+    before { FormSection.all.each &:destroy }
 
     it "should show form section description and help text" do
-      @form_section = FormSection.create_new_custom "Basic Form",
+      form_section = FormSection.create_new_custom "Basic Form",
                                                     "This is a description for basic form", 
                                                     "Help text for basic form"
-      assigns[:form_sections] = [@form_section]
+      assigns[:form_sections] = [form_section]
 
-      render :locals => { :form_section => @form_section }
+      render :locals => { :form_section => form_section }
     
       response.should have_tag(".form-section-description")
       response.should have_tag(".form-section-help-text")
     end
     
     it "should show form section description but no help text" do
-      @form_section = FormSection.create_new_custom "Basic Form","This is a description for basic form", nil
-      assigns[:form_sections] = [@form_section]
+      form_section = FormSection.create_new_custom "Basic Form","This is a description for basic form", nil
+      assigns[:form_sections] = [form_section]
 
-      render :locals => { :form_section => @form_section }
+      render :locals => { :form_section => form_section }
     
       response.should have_tag(".form-section-description")
       response.should_not have_tag(".form-section-help-text")
     end
   
     it "should show help text but no form section description" do
-      @form_section = FormSection.create_new_custom "Basic Form", nil, "This is some help text"
-      assigns[:form_sections] = [@form_section]
+      form_section = FormSection.create_new_custom "Basic Form", nil, "This is some help text"
+      assigns[:form_sections] = [form_section]
 
-      render :locals => { :form_section => @form_section }
+      render :locals => { :form_section => form_section }
     
       response.should_not have_tag(".form-section-description")
       response.should have_tag(".form-section-help-text")
     end
     
     it "should show neither" do
-      @form_section = FormSection.create_new_custom "Basic Form", nil, nil
-      assigns[:form_sections] = [@form_section]
+      form_section = FormSection.create_new_custom "Basic Form", nil, nil
+      assigns[:form_sections] = [form_section]
 
-      render :locals => { :form_section => @form_section }
+      render :locals => { :form_section => form_section }
     
       response.should_not have_tag(".form-section-description")
       response.should_not have_tag(".form-section-help-text")
