75f024c29649791d2b62e804aab5aa7c24781037
zoodor <mtaylor@thoughtworks.com>
2011-03-28 19:49:50 +0100

    Moved default time zone to user class.

--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -2,7 +2,7 @@ class UserPreferencesController < ApplicationController
 
   def update
 
-    @user = User.find_by_user_name(params[:id])
+    @user = User.find_by_user_name(current_user_name)
 
     if @user.update_attributes(params[:user])
       flash[:notice] = 'Timezone was successfully updated.'
