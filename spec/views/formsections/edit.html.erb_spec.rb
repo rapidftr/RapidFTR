c01adcf4ff5978ca9f75afa1867c50dbf94860c7
Jang-Soo Lee <jangsoo.lee@gmail.com>
2011-04-20 10:10:22 -0700

    Added failing test

--- spec/views/formsections/edit.html.erb_spec.rb
+++ spec/views/formsections/edit.html.erb_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 require 'nokogiri'
 
 describe "form_section/edit.html.erb" do
+  it "should not allow to edit name for permanently enabled formsections" do
+    fields = [Field.new :name => 'my_field', :display_name => 'My Field', :enabled => true]
+    form_section = FormSection.new "name" => "Basic Details", "enabled"=> "true", "description"=>"Blah blah", "order"=>"10", "unique_id"=> "basic_details", :editable => "false", :fields => fields, :perm_enabled => true
+
+    assigns[:form_section] = form_section
+    render
+
+    document = Nokogiri::HTML(response.body)
+    document.css("#form_section_name").should be_empty
+  end
   it "should not allow to enable/disable fields for non editable formsections" do
     fields = [Field.new :name => 'my_field', :display_name => 'My Field', :enabled => true]
     form_section = FormSection.new "name" => "Basic Details", "enabled"=> "true", "description"=>"Blah blah", "order"=>"10", "unique_id"=> "basic_details", :editable => "false", :fields => fields
