3895b48b1b6e22fe870751f3db1c1d5f3dfab119
ptodkar <ThoughtWorks@Praful-Todkars-MacBook-Pro.local>
2011-06-07 22:04:48 -0400

    Praful: #708 - Added cucumber specs for unflagging functionality; moved ruby code into helper

--- spec/helpers/histories_helper_spec.rb
+++ spec/helpers/histories_helper_spec.rb
@@ -22,4 +22,21 @@ describe HistoriesHelper do
     
     view.history_wording(nil, "New York").should == "initially set to New York"
   end
+
+  describe "#flag_change_message" do
+    before do
+    @view = Object.new
+    @view.extend(HistoriesHelper)
+    end
+
+    it "should get the flag change message from the history" do
+        history = {'changes' => {'flag_message' => {'to' => 'message'}}}
+        @view.flag_change_message(history).should == 'message'
+    end
+
+    it "should return an empty string if no changes have been made" do
+      history = {'changes' => {}}
+      @view.flag_change_message(history).should == ''
+    end
+  end
 end
