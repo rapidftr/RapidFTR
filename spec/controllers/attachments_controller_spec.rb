8bba13e55d846f0476e9f994b2290873e348d6db
Carlos Júnior <me@xjunior.me>
2011-07-05 15:59:12 -0300

    Fixing test issue on attachments_controller_spec

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -25,9 +25,8 @@ describe AttachmentsController do
     Time.stub!(:now).and_return Time.parse("Jan 20 2010 12:04:24")
     child = Child.create('last_known_location' => "New York", 'photo' => uploadable_photo_jeff)
 
-    created_child = Child.get(child.id)
     Time.stub!(:now).and_return Time.parse("Feb 20 2010 12:04")
-    created_child.update_attributes :photo => uploadable_photo
+    Child.get(child.id).update_attributes :photo => uploadable_photo
 
     get :show, :child_id => child.id, :id => child.primary_photo.name
 
@@ -36,15 +35,14 @@ describe AttachmentsController do
   end
   
   it "should return correct photo size that is older than the current one" do
-    pending "not sure why size is off for this test but not the one above ... works in UI"
     Time.stub!(:now).and_return Time.parse("Jan 20 2010 12:04:24")
     child = Child.create('last_known_location' => "New York", 'photo' => uploadable_photo_jeff)
 
-    Time.stub!(:now).and_return Time.parse("Feb 20 2010 12:04:24")
-    child.update_attributes :photo => uploadable_photo
+    Time.stub!(:now).and_return Time.parse("Feb 20 2010 12:04")
+    Child.get(child.id).update_attributes :photo => uploadable_photo
 
-    get :show, :child_id => child.id, :id => 'photo-2010-01-20T120424'
+    get :show, :child_id => child.id, :id => child.primary_photo.name
 
-    response.body.size.should == uploadable_photo_jeff.size
+    response.body.size.should == uploadable_photo_jeff.read.size
   end
 end
