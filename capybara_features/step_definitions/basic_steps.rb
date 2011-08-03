8526921156583eaeca3738351ff17f0cb20e52af
Jennifer Smith <jennifer@home>
2011-08-02 21:26:17 -0500

    Migrated CSV export features over to capybara

---
+++ capybara_features/step_definitions/basic_steps.rb
@@ -0,0 +1,14 @@
+require 'spec/spec_helper'
+
+When /^the date\/time is "([^\"]*)"$/ do |datetime|
+  current_time = Time.parse(datetime)
+  current_time.stub!(:getutc).and_return Time.parse(datetime)
+  Time.stub!(:now).and_return current_time
+end
+
+When /^the local date\/time is "([^\"]*)" and UTC time is "([^\"]*)"$/ do |datetime, utcdatetime|
+  current_time = Time.parse(datetime)
+  current_time_in_utc = Time.parse(utcdatetime)
+  Time.stub!(:now).and_return current_time
+  current_time.stub!(:getutc).and_return current_time_in_utc
+end
--- capybara_features/step_definitions/login_steps.rb
+++ capybara_features/step_definitions/login_steps.rb
@@ -6,8 +6,24 @@ Given /^I am logged in$/ do
   Given "I press \"Log in\""
 end
 
+Given /^I am logged in as an admin$/ do
+  Given "there is a admin"
+  Given "I am on the login page"
+  Given "I fill in \"admin\" for \"user_name\""
+  Given "I fill in \"123\" for \"password\""
+  Given "I press \"Log in\""
+end
+
+
+
 Given /there is a User/ do
   unless @user
     Given "a user \"mary\" with a password \"123\""
   end
 end
+
+Given /^there is a admin$/ do
+	Given "a admin \"admin\" with a password \"123\""
+end
+
+
