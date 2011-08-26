bb463974c259b1a830707743646a484283e38363
Jean D'Amore <jdamore@thoughtworks.com>
2011-08-26 14:58:07 +1000

    Defect#769: Hidden form fields in PDF - Moved Cucumber tests to capybara_features

--- capybara_features/step_definitions/basic_steps.rb
+++ capybara_features/step_definitions/basic_steps.rb
@@ -1,5 +1,34 @@
 require 'spec/spec_helper'
 
+Given /^the following form sections exist in the system:$/ do |form_sections_table|
+  FormSection.all.each {|u| u.destroy }
+
+  form_sections_table.hashes.each do |form_section_hash|
+    form_section_hash.reverse_merge!(
+      'unique_id'=> form_section_hash["name"].gsub(/\s/, "_").downcase,
+      'enabled' => true,
+      'fields'=> Array.new
+    )
+
+    form_section_hash["order"] = form_section_hash["order"].to_i
+    FormSection.create!(form_section_hash)
+  end
+end
+
+Given /^the following fields exists on "([^"]*)":$/ do |form_section_name, table|
+  form_section = FormSection.get_by_unique_id(form_section_name)
+  form_section.should_not be_nil
+  form_section.fields = []
+  table.hashes.each do |field_hash|
+    field_hash.reverse_merge!(
+      'enabled' => true,
+      'type'=> Field::TEXT_FIELD
+    )
+    form_section.fields.push Field.new(field_hash)
+  end
+  form_section.save!
+end
+
 When /^the date\/time is "([^\"]*)"$/ do |datetime|
   current_time = Time.parse(datetime)
   current_time.stub!(:getutc).and_return Time.parse(datetime)
