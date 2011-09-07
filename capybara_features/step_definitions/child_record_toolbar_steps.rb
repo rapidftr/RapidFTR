5955c1826661814bf462decf33a2b9f073fe53cd
chrisgeorge <chrisfgeorge@gmail.com>
2011-09-06 22:42:05 -0400

    cg - #722 - simplifying cucumber step

--- capybara_features/step_definitions/child_record_toolbar_steps.rb
+++ capybara_features/step_definitions/child_record_toolbar_steps.rb
@@ -8,17 +8,5 @@ Then /^I should see the following links in the toolbars:$/ do |links_table|
 end
 
 When /^I click the "(.*)" button$/ do |button_value|
-  divs = page.all "//div[@class=\"mark-as-form\"]"
-  divs.each do |div|
-    if div.visible?
-      buttons = div.all "//input[@value=\"#{button_value}\"]"
-      buttons.each do |button|
-        if button.visible?
-          button.click
-          break
-        end
-      end
-      break
-    end
-  end
+  click_button button_value
 end
