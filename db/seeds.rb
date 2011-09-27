425605f0de0c67458e4b5af3369c4b3735aa0b6d
John Hume <duelin.markers@gmail.com>
2011-09-26 22:43:00 -0400

    Remove misleading default comment about db:setup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake db:seed.
 
 def should_seed? model
   empty = model.all.empty?
