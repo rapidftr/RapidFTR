f19c7f57a921a149fb259d35c94fec9977d79011
Bruce Lee <jangie@ubuntu.(none)>
2011-09-27 19:20:18 -0700

    Makes field ordering done in same method as form ordering. Had to remove tests, will have to add similar tests as the forms one when those are added.

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ ActionController::Routing::Routes.draw do |map|
   map.enable_form 'form_section/enable', :controller => 'form_section', :action => 'enable', :value => true, :conditions => {:method => :post}
   map.disable_form 'form_section/disable', :controller => 'form_section', :action => 'enable', :value => false
   map.save_order "/form_section/save_order", :controller => "form_section", :action => "save_order"
+  map.save_order_single "/form_section/save_order_single", :controller => "form_section", :action => "save_order_single"
 
   map.session_active '/active', :controller => 'sessions', :action => 'active'
   map.resources :formsections, :controller=>'form_section' do |form_section|
