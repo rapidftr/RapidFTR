d510227af88d0f19dd8f631c53c04ba7909bd430
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2011-12-07 15:57:18 +0530

    #675 Issuing password reset from Blackberry. Included the support to respond through json for any given request.

--- app/controllers/password_recovery_requests_controller.rb
+++ app/controllers/password_recovery_requests_controller.rb
@@ -7,16 +7,36 @@ class PasswordRecoveryRequestsController < ApplicationController
   end
 
   def create
-    if PasswordRecoveryRequest.create params[:password_recovery_request]
-      flash.now[:notice] = "Thank you. A RapidFTR administrator will contact you shortly. If possible, contact the admin directly."
-    else
-      render :new
+    success_notice = "Thank you. A RapidFTR administrator will contact you shortly. If possible, contact the admin directly."
+    failure_notice =  "This user name does not exist. Please try again."
+    respond_to do |format|
+      if is_valid_user and PasswordRecoveryRequest.create params[:password_recovery_request]
+        format.html { flash.now[:notice] = success_notice }
+        format.json { render :json => password_recovery_json(success_notice) , :status => :ok, :head => :ok}
+      else
+        format.html { flash[:error] = failure_notice
+                      redirect_to :action => "new"
+        }
+        format.json { render :json => password_recovery_json(failure_notice) , :status => :ok}
+      end
     end
   end
 
   def hide
     PasswordRecoveryRequest.get(params[:password_recovery_request_id]).hide!
-    flash[:notice] = 'Password request notification was succefully hidden.'
+    flash[:notice] = 'Password request notification was successfully hidden.'
     redirect_to root_path
   end
+
+  private
+  def is_valid_user
+    !params[:password_recovery_request][:user_name].blank? and User.find_by_user_name(params[:password_recovery_request][:user_name])
+  end
+
+  def password_recovery_json(notice)
+    {
+        :response => notice
+    }
+  end
+
 end
--- spec/controllers/password_recovery_requests_controller_spec.rb
+++ spec/controllers/password_recovery_requests_controller_spec.rb
@@ -2,10 +2,15 @@ require 'spec_helper'
 
 describe PasswordRecoveryRequestsController do
 
-  it "should create password recovery request" do
-    PasswordRecoveryRequest.should_receive(:create).with({"username" => "ygor"}).and_return(mock)
+    def mock_user(stubs={})
+    @mock_user ||= mock_model(User, stubs)
+    end
 
-    post :create, {:password_recovery_request => {:username => "ygor"}}
+  it "should create password recovery request" do
+    User.should_receive(:find_by_user_name).with("ygor").and_return(mock_user)
+    PasswordRecoveryRequest.should_receive(:create).with({"user_name" => "ygor"}).and_return(mock)
+
+    post :create, {:password_recovery_request => {:user_name => "ygor"}}
 
     response.flash[:notice].should == "Thank you. A RapidFTR administrator will contact you shortly. If possible, contact the admin directly."
 
