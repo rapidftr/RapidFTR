b0434635ab2df1fd77acb4fab9855ab6fea3584b
Ganesh <ganesh.ramana@gmail.com>
2012-05-02 00:28:33 -0600

    1143: Upgrade Assorted Admin Features from Webrat to Capybara

--- capybara_features/step_definitions/web_steps.rb
+++ capybara_features/step_definitions/web_steps.rb
@@ -9,7 +9,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "pat
 
 module WithinHelpers
   def with_scope(locator)
-    locator ? within(locator) { yield } : yield
+    locator ? within(:css, locator) { yield } : yield
   end
 end
 World(WithinHelpers)
