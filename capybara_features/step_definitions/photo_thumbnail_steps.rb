af4250d8d4bc28bb9dfd15febff9db345d5389d8
Mark T <mtaylor@thoughtworks.com>
2012-06-19 17:05:05 -0700

    Story #1146 - MT - Migrated the basic_search feature from Webrat to Capybara.

---
+++ capybara_features/step_definitions/photo_thumbnail_steps.rb
@@ -0,0 +1,13 @@
+require 'spec/spec_helper'
+require 'spec/support/matchers/attachment_response'
+include CustomMatchers
+
+Then /^I should see the thumbnail of "([^\"]*)"$/ do |child_name|
+  check_link(response, child_name) {|child| child_thumbnail_path(child, child.primary_photo_id)}
+end
+
+def check_link(response, child_name)
+  child = find_child_by_name child_name
+  image_link = yield(child)
+  page.body.should have_xpath("//img[@src='#{image_link}']")
+end
