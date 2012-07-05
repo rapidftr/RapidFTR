1466bd768aa3c18af83445b04ecbd782625fe231
Mark T <mtaylor@thoughtworks.com>
2012-07-05 13:22:15 -0700

    Story #1147 - MT - Migrated the view_child_record_log feature from Webrat to Capybara.

--- capybara_features/step_definitions/photo_thumbnail_steps.rb
+++ capybara_features/step_definitions/photo_thumbnail_steps.rb
@@ -10,6 +10,12 @@ Then /^I should see the thumbnail of "([^\"]*)"$/ do |child_name|
   check_link(response, child_name) {|child| child_thumbnail_path(child, child.primary_photo_id)}
 end
 
+Then /^I should see the thumbnail of "([^\"]*)" with timestamp "([^"]*)"$/ do |name, timestamp|
+  thumbnail = all("//img[@alt='#{name}' and contains(@src,'#{timestamp}')]").first
+  thumbnail.should_not be_nil
+  thumbnail['src'].should =~ /photo.*-#{timestamp}/
+end
+
 Then /^I should see "([^\"]*)" thumbnails$/ do |number|
   thumbnails = all("//*[@class='thumbnail']/img")
   thumbnails.collect{|element| element['src']}.uniq.size.should eql number.to_i
