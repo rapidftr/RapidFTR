65738a064bf449064dfeeebd45b4d608b7c27da7
Maxim Filimonov <tpaktopsp@gmail.com>
2012-07-05 15:53:12 +1000

    Adding spec for radio button not to be present when user is editng himself

---
+++ spec/views/users/_edittable_user.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'users/_edittable_user.html.erb' do
+  describe "User permission level" do
+    it "should not be allowed to be updated by user" do
+      user = User.new()
+      user.disabled = false
+      user.user_name = "someusername"
+      
+      render :locals => { :current_user_name => "someusername", :edittable_user => user }
+
+      response.body.should_not =~ /<input.*user\[permission\].*type="radio"/
+    end
+  end
+end
