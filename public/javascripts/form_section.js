ddc8787aea4cad24bee3decd8e88237b2058f2fe
Minno Dang <mndang@thoughtworks.com>
2012-07-11 21:38:32 +0100

    #611|Minno|fixed bug with field reordering after javascript refactoring

--- public/javascripts/form_section.js
+++ public/javascripts/form_section.js
@@ -72,6 +72,41 @@ function moveDown() {
 }
 
 function saveOrder(event) {
+    var form_order = {};
+    var updatedOrderings = $('.updatedFormSectionOrder :input');
+    $.each(updatedOrderings, function() {
+	var name = $(this).attr("name");
+	var id = /form_order\[(.*)\]/.exec(name)[1]
+	form_order[id] = $(this).attr("value");
+    });
+    var url='';
+    var formId='';
+    if ($('#editFormDetails').length === 0){
+	url = '/form_section/save_order';
+    }else{
+	url = '/form_section/save_order_single';
+	formId = $('#sectionId').html();
+    }
+
+
+    $.ajax({
+	type: "POST",
+	data: {"form_order" : form_order,
+	       "formId" : formId},
+	url: url,
+	success: function(data) {
+	    if ($('#form_sections').length === 1){
+		$("#form_sections").html($(data).find("#form_sections"));
+		$("a.moveDown").bind("click", moveDown);
+		$("a.moveUp").bind("click", moveUp);
+		initOrderingColumns();
+            $("#successNotice").show();
+	    }
+	}
+    });
+}
+
+function saveOrder(event) {
     var form_order = getUpdatedOrderings('.updatedFormSectionOrder :input');
 
     $.ajax({
@@ -91,7 +126,7 @@ function saveOrder(event) {
 }
 
 function saveFieldOrder(event) {
-    var form_order = getUpdatedOrderings('.updatedFormSectionFieldOrder :input');
+    var form_order = getUpdatedOrderings('.updatedFormSectionOrder :input');
     var formId = $('#sectionId').html();
 
     $.ajax({
