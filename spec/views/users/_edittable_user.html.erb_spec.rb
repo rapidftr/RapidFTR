82573c2febf2cf252dfe57bc7fcce01d585a978c
Maxim Filimonov <tpaktopsp@gmail.com>
2012-07-15 13:22:29 +1000

    Fix scope of permission regex

--- spec/views/users/_edittable_user.html.erb_spec.rb
+++ spec/views/users/_edittable_user.html.erb_spec.rb
@@ -1,28 +1,28 @@
 require 'spec_helper'
-permission_regex = /<input .*user\[permission\].*>/
 describe 'users/_edittable_user.html.erb' do
-	describe "User permission level" do
-		before :each do
-			@user = User.new()
-			@user.disabled = false
-		end 
-		it "should not be updateable" do
-			@controller.template.stub!(:is_admin?).and_return(false)
+  permission_regex = /<input .*user\[permission\].*>/
+  describe "User permission level" do
+    before :each do
+      @user = User.new()
+      @user.disabled = false
+    end 
+    it "should not be updateable" do
+      @controller.template.stub!(:is_admin?).and_return(false)
 
-			render :locals => { :edittable_user => @user }
+      render :locals => { :edittable_user => @user }
 
-			permission_regex.match(response.body).to_a.each do |p|
-				p.should include("disabled")	
-			end
-		end
-		it "should be allowed to be updated by admin" do
-			@controller.template.stub!(:is_admin?).and_return(true)
+      permission_regex.match(response.body).to_a.each do |p|
+        p.should include("disabled")	
+      end
+    end
+    it "should be allowed to be updated by admin" do
+      @controller.template.stub!(:is_admin?).and_return(true)
 
-			render :locals => { :edittable_user => @user }
+      render :locals => { :edittable_user => @user }
 
-			permission_regex.match(response.body).to_a.each do |p|
-				p.should_not include("disabled")	
-			end
-		end
-	end
+      permission_regex.match(response.body).to_a.each do |p|
+        p.should_not include("disabled")	
+      end
+    end
+  end
 end
