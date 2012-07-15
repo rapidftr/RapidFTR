1561790bb3d7b5ea30786bde685c6a7d37c02619
Maxim Filimonov <tpaktopsp@gmail.com>
2012-07-15 23:08:27 +1000

    Refactor regex matchers

--- spec/views/users/_edittable_user.html.erb_spec.rb
+++ spec/views/users/_edittable_user.html.erb_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 describe 'users/_edittable_user.html.erb' do
-  permission_regex = /<input .*user\[permission\].*>/
   describe "User permission level" do
     before :each do
       @user = User.new()
@@ -11,17 +10,19 @@ describe 'users/_edittable_user.html.erb' do
 
       render :locals => { :edittable_user => @user }
 
-      permission_regex.match(response.body).to_a.each do |p|
-        p.should include("disabled")	
-      end
+      permissions {|p| p.should include("disabled") }
     end
     it "should be allowed to be updated by admin" do
       @controller.template.stub!(:is_admin?).and_return(true)
 
       render :locals => { :edittable_user => @user }
 
+      permissions {|p| p.should_not include("disabled") }
+    end
+    def permissions(&block)
+      permission_regex = /<input .*user\[permission\].*>/
       permission_regex.match(response.body).to_a.each do |p|
-        p.should_not include("disabled")	
+        block.call(p)
       end
     end
   end
