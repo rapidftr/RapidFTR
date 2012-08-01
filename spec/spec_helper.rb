fa9fa07937d5d31a492c68abf8fd1eb368f0d10a
John Hume <duelin.markers@gmail.com>
2012-07-31 21:44:27 -0400

    Bring back a bunch of file-upload spec helper methods - #1177

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+
+  config.include UploadableFiles
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
