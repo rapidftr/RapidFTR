e09ad5fc3af53d9a8c18695f31cf7ad8069590f8
Ashok <ashokk@thoughtworks.com>
2012-09-05 14:49:33 +0530

    #1175 - Ashok - Fixed the broken search(removed the block condition as the regex itself will ensure the query is not empty) also removed the &nbsp for layout

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,17 +1,14 @@
 class Search
   include Validatable
-  
+
   attr_accessor :query
-  
+
   validates_presence_of :query
   validates_length_of :query, :maximum => 150
-  validates_format_of :query, 
-    :with => %r{^([\w ]+)$},
-    :if => lambda { !query.blank?},
-    :message => "must only be letters (a to z) or numbers (0-9). Please try again with a different key word."
-  
+  validates_format_of :query, :with => /^([\w ]+)$/, :message => "must only be letters (a to z) or numbers (0-9). Please try again with a different key word."
+
   def initialize(query)
     @query = query.strip
   end
-  
+
 end
