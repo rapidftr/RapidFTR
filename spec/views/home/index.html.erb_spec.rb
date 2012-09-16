983d7153ee0627440d65fbaec7d84aec6688dbbe
John Hume <duelin.markers@gmail.com>
2012-09-16 12:45:51 -0500

    Hopefully fix examples failing on TeamCity but not locally

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "home/index.html.erb" do
   it "should display time for notifications in yyyy/mm/dd format for users who do not already exist" do
-    mock_password_recovery_request = mock("PasswordRecoveryRequest", :user_name =>'mjhasson', :created_at => Time.parse("Jan 31 2011"))
+    mock_password_recovery_request = mock("PasswordRecoveryRequest", :user_name =>'mjhasson', :created_at => Time.parse("Jan 31 2011"), :to_param => '123')
     assign(:notifications,[ mock_password_recovery_request ])
 
     view.stub!(:is_admin?).and_return(true)
@@ -12,7 +12,7 @@ describe "home/index.html.erb" do
   end
 
   it "should display time for notifications in yyyy/mm/dd format for users who do exist" do
-    mock_password_recovery_request = mock("PasswordRecoveryRequest", :user_name =>'jpretorius', :created_at => Time.parse("Jan 31 2011"))
+    mock_password_recovery_request = mock("PasswordRecoveryRequest", :user_name =>'jpretorius', :created_at => Time.parse("Jan 31 2011"), :to_param => '124')
     assign(:notifications, [ mock_password_recovery_request ])
 
     # to_param is because the CI build is failing when trying to generate named route for this mock object - CG Nov 24 2011
