e451b42101ebc10366081b910556fdf4feb5b608
Ashok <ashokk@thoughtworks.com>
2012-10-12 13:56:27 +0530

    #1215 - Akash/Ashok - Added unique validation for role name

--- app/models/role.rb
+++ app/models/role.rb
@@ -8,8 +8,22 @@ class Role < CouchRestRails::Document
   property :description
   property :permissions, :type => [String]
 
+  view_by :name,
+    :map => "function(doc) {
+              if ((doc['couchrest-type'] == 'Role') && doc['name'])
+             {
+                emit(doc['name'],doc);
+             }
+          }"
+
   validates_presence_of :name
   validates_presence_of :permissions, :message => "Please select at least one permission"
+  validates_with_method :name, :method => :is_name_unique, :if => :name
+
+  def is_name_unique
+    return true if Role.by_name(:key => name).empty?
+    [false, "A role with that name already exists, please enter a different name"]
+  end
 
 end
 
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -13,6 +13,13 @@ describe Role do
     role.errors.on(:permissions).should == ["Please select at least one permission"]
   end
 
+  it "should not be valid if a role name has been taken already" do
+    Role.create({:name => "Unique", :permissions => [Permission::ADMIN]})
+    role = Role.new({:name => "Unique", :permissions => [Permission::ADMIN]})
+    role.should_not be_valid
+    role.errors.on(:name).should == ["A role with that name already exists, please enter a different name"]
+  end
+
   it "should create a valid role" do
     Role.new(:name => "some_role", :permissions => [Permission::ADMIN]).should be_valid
   end
