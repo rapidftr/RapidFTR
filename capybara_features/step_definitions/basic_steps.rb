9dfdcf71c9f0c08b0f8f9231cc71d7345fb0fb53
Jeyakar <j.jeyakarvaiz@gmail.com>
2012-10-17 13:01:07 +0530

    Jeyakar - New Layout implementation - Roles pages

--- capybara_features/step_definitions/basic_steps.rb
+++ capybara_features/step_definitions/basic_steps.rb
@@ -115,7 +115,7 @@ Then /^I should see (\d*) divs with text "(.*)" for class "(.*)"$/ do |quantity,
 end
 
 Then /^the "([^\"]*)" button presents a confirmation message$/ do |button_name|
-  page.find("//p[@class='#{button_name.downcase}Button']/a")['data-confirm'].should_not be_nil
+  page.find("//a[@class='link_#{button_name.downcase}']")['data-confirm'].should_not be_nil
 end
 
 Given /^I flag "([^\"]*)" as suspect$/ do  |name|
