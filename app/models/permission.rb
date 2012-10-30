aed3329c92f9e0b423eb0a4922b4814d0605451f
kavithatw <kavithar@thoughtworks.com>
2012-10-30 11:37:36 +0530

    #1350 Prasanna/Kavitha Merged Permission file

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -12,7 +12,7 @@ class Permission
   ADMIN    = Permission.to_ordered_hash2({:admin, "Admin"})
   CHILDREN = Permission.to_ordered_hash2({:register => "Register Child"}, {:edit => "Edit Child"}, {:access_all_data => "Access all data"})
   FORMS    = Permission.to_ordered_hash2({})
-  USERS    = Permission.to_ordered_hash2({:create_and_edit => "Create and Edit Users"},{:view => "View Users"})
+  USERS    = Permission.to_ordered_hash2({:create_and_edit => "Create and Edit Users"},{:view => "View Users"},{:destroy => "Delete Users"},{:disable => "Disable Users"})
   DEVICES  = Permission.to_ordered_hash2({})
   REPORTS  = Permission.to_ordered_hash2({})
 
