b0a958be4c528948f2e7ab7cb827390241f15ceb
kavithatw <kavithar@thoughtworks.com>
2012-11-01 12:33:15 +0530

    #1350 Prasanna/Kavitha Added a confirm popup before blacklisting a device

--- public/javascripts/manage_devices.js
+++ public/javascripts/manage_devices.js
@@ -1,14 +1,47 @@
 $(document).ready(function () {
-    $(".device_blacklist_check_box").change(updateBlackListFlag)
+    $(".device_blacklist_check_box").click(function (event) {
+        setModalText();
+        var fail = false;
+        $("#modal-dialog").dialog({
+            modal:true,
+            width:400,
+            title:'Blacklist Device',
+            height:200,
+            buttons:{
+                'Yes':function () {
+                    $(this).dialog('close');
+                    updateBlackListFlag();
+                },
+                'No':function () {
+                    $(this).dialog('close');
+                    fail = true;
+                }
+            }
+        });
+        event.preventDefault();
+    })
 });
 
 function updateBlackListFlag() {
+    var $checkbox = $(".device_blacklist_check_box");
     $.ajax({
         url:"/devices/update_blacklist",
         type:"POST",
-        data:{blacklisted:$(this).is(':checked'), id:$(this).attr('id')},
-        success:function () {
-            alert("Successfully Updated");
+        dataType:'json',
+        data:{blacklisted:!($checkbox.is(':checked')), id:$checkbox.attr('id')},
+        success:function (json) {
+            if (json.status == 'ok') {
+                $checkbox.attr('checked', !$checkbox.attr('checked'));
+            }
         }
     })
+}
+
+function setModalText() {
+    var next_state__as_checked = $(".device_blacklist_check_box").is(':checked');
+    if (next_state__as_checked) {
+        $("#modal-dialog").text("Do you want to add this device to blacklist?");
+    } else {
+        $("#modal-dialog").text("Do you want to remove this device from blacklist?");
+    }
 }
