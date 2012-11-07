33a8cacf9230e87b91a74c12b1af004e961cfefd
Mashell <mashell@server.com>
2012-11-07 16:47:17 +0000

    #1391 -'Flag as Suspect Record' to be renamed to 'Flag Record'

--- capybara_features/step_definitions/basic_steps.rb
+++ capybara_features/step_definitions/basic_steps.rb
@@ -231,6 +231,6 @@ private
 def click_flag_as_suspect_record_link_for(name)
   child = find_child_by_name name
   visit children_path+"/#{child.id}"
-  click_link("Flag as suspect record")
+  click_link("Flag Record")
 end
 
