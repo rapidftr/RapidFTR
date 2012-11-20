20ef0008d32876a5451a588aaf8ffadf71bd6fc2
zoodor <zoodor@hotmail>
2012-11-20 06:41:37 +0000

    #1372 - Mark - Fixing typo in test name.

--- spec/models/search_criteria_spec.rb
+++ spec/models/search_criteria_spec.rb
@@ -121,7 +121,7 @@ describe SearchCriteria do
       SearchCriteria.lucene_query(criteria_list).should == "(QUERY1 AND QUERY2) OR ((QUERY3 AND QUERY4) AND QUERY5)"
   end
 
-  it "should build queyr for criteria without query" do
+  it "should build query for criteria without query" do
     criteria_list = [
       mock(:join => "", :to_lucene_query => ""), 
       mock(:join => "AND", :to_lucene_query => "CRITERIA")
