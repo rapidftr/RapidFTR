b8eb9bc01329fcc1503ffd36ea88a9fc78fe3750
Subhas Dandapani <rdsubhas@thoughtworks.com>
2012-11-22 00:23:32 +0300

    ci | Subhas | Remove unnecessary tasks like logging, timing, middleware, etc from cukes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,6 @@
 RapidFTR::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # The production environment is meant for finished, "live" apps.
-  # Code is not reloaded between requests
-  config.cache_classes = true
-
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
@@ -43,7 +35,4 @@ RapidFTR::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
 end
