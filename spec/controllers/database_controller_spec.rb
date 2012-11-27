0d9d7e90403b4aed2d308d1e0ba08d686ab03da0
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2012-11-27 16:46:02 +0530

    [Refactor] Yekkanti - Removed failing rails env test for database integration

--- spec/controllers/database_controller_spec.rb
+++ spec/controllers/database_controller_spec.rb
@@ -5,9 +5,6 @@ describe DatabaseController do
     fake_admin_login
   end
 
-  after do
-    Rails.env = "test"
-  end
   it "should delete all children in android environment" do
     Rails.env = "android"
     Child.create('last_known_location' => "London")
@@ -15,12 +12,4 @@ describe DatabaseController do
     delete :delete_children
     Child.all.should be_empty
   end
-
-  it "should not delete children if ran in any rails environment other than android" do
-    Child.create('last_known_location' => "London")
-    Child.create('last_known_location' => "India")
-    delete :delete_children
-    Child.all.should_not be_empty
-  end
-
 end
