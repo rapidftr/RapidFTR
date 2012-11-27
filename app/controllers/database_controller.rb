2181b1e425161f520f8ba734cdc87bcb41da2184
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2012-11-27 16:02:18 +0530

    [Refactoring] Changed the render messages appropriately

--- app/controllers/database_controller.rb
+++ app/controllers/database_controller.rb
@@ -5,8 +5,11 @@ class DatabaseController < ApplicationController
       Child.all.each do |child|
         child.destroy
       end
+      render :text => "Deleted all child documents"
+    else
+      render :text => "Operation not allowed in #{Rails.env} environment"
     end
-    render :text => "Reseeded the database"
+
   end
 
 end
