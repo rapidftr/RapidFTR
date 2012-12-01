e9182950c4868366a71131d0124440194c1a3614
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2012-12-02 00:16:04 +0530

    [CI] Yekkanti | added back sudo for nginx reload. Added the default admin user to have permissions to run nginx reload without password in remote machine sudoers file

--- config/recipes/deploy.rb
+++ config/recipes/deploy.rb
@@ -8,7 +8,7 @@ namespace :deploy do
   desc "Restart passenger"
   task :restart do
     run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
-    run "/opt/local/nginx/sbin/nginx -s reload"
+    sudo "/opt/local/nginx/sbin/nginx -s reload"
   end
 
   desc "Create releases and log folder"
