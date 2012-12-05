328dd76d89e44fcbc21516fa631b85fca1027239
Subhas Dandapani <subhasda@thoughtworks.com>
2012-12-05 11:31:33 +0300

    1444 | Subhas | First round of purging admin permission from system
    Conflicts:
    	app/controllers/roles_controller.rb

--- spec/support/matchers/ability.rb
+++ spec/support/matchers/ability.rb
@@ -1,17 +1,43 @@
 RSpec::Matchers.define :authorize do |action, object|
-  match do |actual|
-    actual.can?(action, object).should == true
+  match do |ability|
+    ability.can? action, object
+  end
+
+  failure_message_for_should do |ability|
+    "Expected Permissions #{ability.user.permissions.inspect} To Authorize #{action.to_s} On #{object.inspect}"
+  end
+
+  failure_message_for_should_not do |ability|
+    "Did not expect User #{ability.user.inspect} To Authorize #{action.to_s} On #{object.inspect}"
   end
 end
 
 RSpec::Matchers.define :authorize_all do |actions, *objects|
-  match do |actual|
-    actions.product(objects).all? { |(action, object)| actual.can?(action, object) }.should == true
+  match do |ability|
+    actions.product(objects).all? do |(action, object)| 
+      @action, @object = [ action, object ]
+      ability.can? action, object
+    end
+  end
+
+  failure_message_for_should do |ability|
+    "Expected User #{ability.user.inspect} To Authorize #{@action.to_s} On #{@object.inspect}"
+  end
+
+  failure_message_for_should_not do |ability|
+    "Did not expect User #{ability.user.inspect} To Authorize #{@action.to_s} On #{@object.inspect}"
   end
 end
 
 RSpec::Matchers.define :authorize_any do |actions, *objects|
-  match do |actual|
-    actions.product(objects).any? { |(action, object)| actual.can?(action, object) }.should == true
+  match do |ability|
+    actions.product(objects).any? do |(action, object)| 
+      @action, @object = [ action, object ]
+      ability.can? action, object
+    end
+  end
+
+  failure_message_for_should_not do |ability|
+    "Did not expect User #{ability.user.inspect} To Authorize #{@action.inspect} On #{@object.inspect}"
   end
 end
