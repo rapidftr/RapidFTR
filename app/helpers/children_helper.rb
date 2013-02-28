0fc19f480534c4857122d3a83738d72e50d21be9
Faris Mohammed <farismosman@gmail.com>
2013-02-28 15:05:48 +0300

    [#1220] Akash/Faris : Refactored by moving partial viewa form child_histories to shared/histories. user_histories is using the same partial views.

--- app/helpers/children_helper.rb
+++ app/helpers/children_helper.rb
@@ -81,7 +81,7 @@ module ChildrenHelper
 
   def history_entry_for history, field, change
     if field == "photo_keys"
-      return {:partial => "photo_history_change",
+      return {:partial => "shared/histories/photo_history_change",
               :locals =>{
                 :new_photos => change['added'],
                 :deleted_photos => change['deleted'],
@@ -90,25 +90,25 @@ module ChildrenHelper
                 :organisation => history['user_organisation']}}
 
     elsif field == 'recorded_audio'
-      return {:partial => "audio_history_change",
+      return {:partial => "shared/histories/audio_history_change",
               :locals => default_locals_for(history, change) }
     elsif field == 'flag'
-      return {:partial => "flag_change",
+      return {:partial => "shared/histories/flag_change",
               :locals => default_locals_for(history, change).merge(:message => new_value_for(history, 'flag_message'))}
     elsif field == 'reunited'
-      return {:partial => "reunited_change",
+      return {:partial => "shared/histories/reunited_change",
               :locals => default_locals_for(history, change).merge(:message => new_value_for(history, 'reunited_message'))}
     elsif field == 'investigated'
-      return {:partial => "investigated_change",
+      return {:partial => "shared/histories/investigated_change",
               :locals => default_locals_for(history, change).merge(:message => new_value_for(history, 'investigated_message'))}
     elsif field == 'duplicate'
-      return {:partial => "duplicate_change",
+      return {:partial => "shared/histories/duplicate_change",
               :locals => default_locals_for(history, change).merge(:duplicate_of => new_value_for(history, 'duplicate_of'))}
     elsif ['flag_message', 'reunited_message', 'investigated_message', 'duplicate_of'].include? field
       return {}
       # do nothing, because we are already displaying the duplicate_of as a part of duplicate change
     else
-    return {:partial => "history_change",
+    return {:partial => "shared/histories/history_change",
             :locals => default_locals_for(history, change).merge(:field => field.humanize)}
     end
   end
