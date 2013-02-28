5fb18da6475f27b330c8be38920b006ae93fbee9
Faris Mohammed <farismosman@gmail.com>
2013-02-28 14:26:41 +0300

    [#1220] Akash/Faris: Added child name and id to histories.

--- app/controllers/user_histories_controller.rb
+++ app/controllers/user_histories_controller.rb
@@ -5,7 +5,7 @@ class UserHistoriesController < ApplicationController
     @page_name = t("history_of")+" #{@user.user_name} " + t("actions")
 
     children = Child.all_connected_with(@user.user_name)
-    unsorted = children.map{|child| child.histories}.flatten
+    unsorted = children.map{|child| child.histories.map{|history| history.merge(:child_id => child.id, :child_name => child.name)}}.flatten
     @histories = unsorted.sort { |that, this| DateTime.parse(this["datetime"]) <=> DateTime.parse(that["datetime"])}
   end
 end
--- spec/controllers/user_histories_controller_spec.rb
+++ spec/controllers/user_histories_controller_spec.rb
@@ -11,15 +11,18 @@ describe UserHistoriesController do
                     {:controller => "user_histories", :action => "index", :id => "1"})
   end
 
-  it "should pass ordered histories for user to view" do
+  it "should pass ordered histories, child name and id for user to view" do
     history1 = {"datetime" => 1.day.ago.to_s}
     history2 = {"datetime" => 1.week.ago.to_s}
     User.should_receive(:get).with("1").and_return(mock(:user_name => (user_name = "some user")))
-    Child.should_receive(:all_connected_with).with(user_name).and_return([mock(:histories => [history2]), mock(:histories => [history1])])
+    Child.should_receive(:all_connected_with).with(user_name).and_return([
+          mock(:id => "other_id", :name => "other_name", :histories => [history2]),
+          mock(:id => "another_id", :name => "another_name", :histories => [history1])
+    ])
 
     get :index, :id => "1"
 
-    assigns(:histories).should == [history1, history2]
+    assigns(:histories).should == [history1.merge("child_id" => "another_id", "child_name" => "another_name"), history2.merge("child_id" => "other_id", "child_name" => "other_name")]
   end
 
   it "should set the page name to the user" do
