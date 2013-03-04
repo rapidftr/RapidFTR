ac88ca01422baee0010f43f44235aca0616ac30f
Suganthi T <suganthk@thoughtworks.com>
2013-03-04 11:38:51 +0530

    1634 - Kavitha/Suganthi - Fixed duplication of change log

--- app/models/child.rb
+++ app/models/child.rb
@@ -516,7 +516,7 @@ class Child < CouchRestRails::Document
   def update_history
     if field_name_changes.any?
       changes = changes_for(field_name_changes)
-      (add_to_history(changes) unless (!self['histories'].empty? && (self['histories'].last["changes"].to_s.include? changes.to_s)))
+      (add_to_history(changes) unless (!self['histories'].empty? && ((self['histories'].last["changes"].to_s.include? changes.to_s) || (changes.to_s.include? self['histories'].last["changes"].to_s))))
     end
   end
 
