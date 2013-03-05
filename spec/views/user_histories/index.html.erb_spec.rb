dfa0ce1a6abab89374514ebdf3a3fefaeb3da822
Faris Mohammed <farismosman@gmail.com>
2013-03-05 10:46:18 +0300

    [#1220] Akash/Faris : fixing the failing test.

--- spec/views/user_histories/index.html.erb_spec.rb
+++ spec/views/user_histories/index.html.erb_spec.rb
@@ -1,22 +1,20 @@
 require 'spec_helper'
 
 describe "user_histories/index.html.erb" do
-  describe "user history" do
 
-    before do
-      @user = mock(:user_name => "Bob")
-    end
+  before do
+    @user = mock(:user_name => "Bob")
+  end
 
-    describe "newly created user" do
-      it "should render user has no activities" do
-        assign(:histories, {})
-        assign(:user,  @user)
+  describe "newly created user" do
+    it "should render user has no activities" do
+      assign(:histories, {})
+      assign(:user,  @user)
 
-        render :template => "user_histories/index.html.erb"
+      render :template => "user_histories/index.html.erb"
 
-        rendered.should have_tag(".history-details") do
-          with_tag("li", /Bob has no activity./)
-        end
+      rendered.should have_tag(".history-details") do
+        with_tag("li", /Bob has no activity./)
       end
     end
   end
