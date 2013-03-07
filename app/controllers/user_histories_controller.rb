3d869cdaf98003b7329a0a8f9522736668cd7266
akash <akash@thoughtworks.com>
2013-03-07 09:23:35 +0300

    1644 - Akash - removing history entries for other users when viewing user history log

--- app/controllers/user_histories_controller.rb
+++ app/controllers/user_histories_controller.rb
@@ -9,5 +9,6 @@ class UserHistoriesController < ApplicationController
     children = Child.all_connected_with(@user.user_name)
     unsorted = children.map{|child| child.histories.map{|history| history.merge(:child_id => child.id, :child_name => child.name)}}.flatten
     @histories = unsorted.sort { |that, this| DateTime.parse(this["datetime"]) <=> DateTime.parse(that["datetime"])}
+    @histories.reject!{|history| history["user_name"]!= @user.user_name}
   end
 end
--- spec/controllers/user_histories_controller_spec.rb
+++ spec/controllers/user_histories_controller_spec.rb
@@ -12,8 +12,8 @@ describe UserHistoriesController do
   end
 
   it "should pass ordered histories, child name and id for user to view" do
-    history1 = {"datetime" => 1.day.ago.to_s}
-    history2 = {"datetime" => 1.week.ago.to_s}
+    history1 = {"user_name" => "some user", "datetime" => 1.day.ago.to_s}
+    history2 = {"user_name" => "some user", "datetime" => 1.week.ago.to_s}
     User.should_receive(:get).with("1").and_return(mock(:user_name => (user_name = "some user")))
     Child.should_receive(:all_connected_with).with(user_name).and_return([
           mock(:id => "other_id", :name => "other_name", :histories => [history2]),
@@ -23,6 +23,20 @@ describe UserHistoriesController do
     get :index, :id => "1"
 
     assigns(:histories).should == [history1.merge("child_id" => "another_id", "child_name" => "another_name"), history2.merge("child_id" => "other_id", "child_name" => "other_name")]
+  end
+
+  it "should remove history entries for other users" do
+    User.should_receive(:get).with("1").and_return(mock(:user_name => (user_name = "some user")))
+    history1 = {"user_name" => "some user", "datetime" => 1.day.ago.to_s}
+    history2 = {"user_name" => "some other user", "datetime" => 1.week.ago.to_s}    
+    Child.should_receive(:all_connected_with).with(user_name).and_return([
+          mock(:id => "other_id", :name => "other_name", :histories => [history2]),
+          mock(:id => "another_id", :name => "another_name", :histories => [history1])
+    ])
+
+    get :index, :id => "1"
+
+    assigns(:histories).should == [history1.merge("child_id" => "another_id", "child_name" => "another_name")]
   end
 
   it "should set the page name to the user" do
