a21882ce1941e15081b2fc76e5feffa56083645a
Subhas Dandapani <subhasda@thoughtworks.com>
2013-03-07 18:10:50 +0530

    1600 | Subhas | Remove special characters from option_strings so that they don't interfere with search

--- db/migration/0006_add_ftr_status_form_field_to_basic_identity.rb
+++ db/migration/0006_add_ftr_status_form_field_to_basic_identity.rb
@@ -4,7 +4,7 @@ existing_ftr_status_field = basic_identity_form_section.fields.find{|field| fiel
 if existing_ftr_status_field
 	existing_ftr_status_field.name = "ftr_status" 
 else
-	basic_identity_form_section.fields << Field.new("name" => "ftr_status", "display_name" => "FTR Status", "type" => "select_box", "option_strings_text" => "Identified\nVerified\nTracing On-Going\nFamily Located-Cross-Border FR Pending\nFamily Located- Inter-Camp FR Pending\nReunited\nExported to CPIMS\nRecord Invalid","highlight_information"=>HighlightInformation.new("highlighted"=>true,"order"=>4))
+	basic_identity_form_section.fields << Field.new("name" => "ftr_status", "display_name" => "FTR Status", "type" => "select_box", "option_strings_text" => "Identified\nVerified\nTracing On-Going\nFamily Located Cross-Border FR Pending\nFamily Located Inter-Camp FR Pending\nReunited\nExported to CPIMS\nRecord Invalid","highlight_information"=>HighlightInformation.new("highlighted"=>true,"order"=>4))
 end
 
 basic_identity_form_section.save
