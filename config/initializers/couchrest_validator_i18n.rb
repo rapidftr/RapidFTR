8115214da30e494939b3b2758a64488b2ca0dc1a
Faris Mohammed <farismosman@gmail.com>
2013-03-12 10:51:42 +0300

    [#translation] Faris : translating couchrest validation messages.

---
+++ config/initializers/couchrest_validator_i18n.rb
@@ -0,0 +1,11 @@
+module CouchRest
+  module Validation
+    class ValidationErrors
+      def self.default_error_message(key, field, *values)
+        field = I18n.translate CouchRest.humanize(field), :scope => "couchrest.fields"
+        translation = I18n.translate key, :scope => "couchrest.validations"
+        translation % [ field, *values ].flatten
+      end
+    end
+  end
+end
