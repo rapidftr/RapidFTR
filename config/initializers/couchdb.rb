9a85af7c8811677ecfddd3e8926b804164107d6f
Subhas Dandapani <subhasda@thoughtworks.com>
2013-03-22 17:15:52 +0530

    1663 | Subhas/Kavitha | Fix Netbook-to-netbook sync

--- config/initializers/couchdb.rb
+++ config/initializers/couchdb.rb
@@ -1,37 +1,23 @@
 begin
 
-  env = ENV['RAILS_ENV'] || 'development'
-
-  couchdb_config = YAML::load(ERB.new(Rails.root.join("config", "couchdb.yml").read).result)[env]
-
-  host      = couchdb_config["host"]      || 'localhost'
-  port      = couchdb_config["port"]      || '5984'
-  database  = couchdb_config["database"]
-  username  = couchdb_config["username"]
-  password  = couchdb_config["password"]
-  ssl       = (couchdb_config["ssl"].blank? or couchdb_config["ssl"] == false) ? false : true
-  db_prefix = couchdb_config["database_prefix"] || ""
-  db_suffix = couchdb_config["database_suffix"] || ""
-  https_port = couchdb_config["https_port"]      || '6984'
-
-  protocol = ssl ? 'https' : 'http'
-  authorized_host = (username.blank? && password.blank?) ? host :
-    "#{CGI.escape(username)}:#{CGI.escape(password)}@#{host}"
-rescue
-  raise "There was a problem with your config/couchdb.yml file. Check and make sure it's present and the syntax is correct.
-         If it is not present copy couchdb.yml.example and save it as couchdb.yml. Do not checkin couchdb.yml(any ways its gitignored)
-         ***************************
-
-         Its recommended to run the rake task db:create_couch_sysadmin if you have fixed couchdb's admin party! "
-else
-
+  settings = CouchSettings.instance
+  
   COUCHDB_CONFIG = {
-      :protocol => protocol,
-      :host_path => "#{protocol}://#{authorized_host}:#{port}",
-      :db_prefix => "#{db_prefix}",
-      :db_suffix => "#{db_suffix}",
-      :https_port => https_port,
+    :host_path => settings.uri.to_s,
+    :db_prefix => settings.db_prefix,
+    :db_suffix => settings.db_suffix
   }
+  COUCHDB_SERVER = CouchRest.new settings.uri.to_s
 
-  COUCHDB_SERVER = CouchRest.new COUCHDB_CONFIG[:host_path]
+rescue => e
+
+  STDERR.puts <<-END
+    There was a problem with your config/couchdb.yml file. Check and make sure it's present and the syntax is correct.
+    If it is not present copy couchdb.yml.example and save it as couchdb.yml. Do not checkin couchdb.yml(any ways its gitignored)
+    ***************************
+    Its recommended to run the rake task db:create_couch_sysadmin if you have fixed couchdb's admin party!
+END
+
+  raise e
+
 end
