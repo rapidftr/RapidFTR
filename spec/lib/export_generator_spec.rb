05059ff02c0fa928cfde90fc5fb11c82396df80c
Subhas Dandapani <subhasda@thoughtworks.com>
2013-04-02 17:59:53 +0530

    ci | Subhas/Kavitha | Correct failing specs

--- spec/lib/export_generator_spec.rb
+++ spec/lib/export_generator_spec.rb
@@ -3,32 +3,38 @@ require 'spec_helper'
 describe ExportGenerator do
   describe "when generating a CSV download" do
     describe "with just a name field" do
-      user = User.new(
-          :user_name=>'user',
-          :full_name=>'name',
-          :organisation=>"UNICEF"
-      )
-      child1 = Child.new_with_user_name(user,
-        '_id' => '3-123451', 'name' => 'Dave', 'unique_identifier' => "xxxy",
-        'photo_url' => 'http://testmachine:3000/some-photo-path/1',
-        'audio_url' => 'http://testmachine:3000/some-audio-path/1',
-        'current_photo_key' => "photo-some-id-1", 'some_audio' => 'audio-some-id-1')
-        child1.create_unique_id
-      child2 = Child.new_with_user_name(user,
-        '_id' => '4-153213', 'name' => 'Mary', 'unique_identifier' => "yyyx", 
-        'photo_url' => 'http://testmachine:3000/some-photo-path/2',
-        'audio_url' => 'http://testmachine:3000/some-audio-path/2',
-        'current_photo_key' => "photo-some-id-2", 'some_audio' => 'audio-some-id-2' )
-      child2.create_unique_id
-      child3 = Child.new_with_user_name(user,
-                                        '_id' => '5-188888', 'name' => 'Jane', 'unique_identifier' => "yxyy",
-                                        'photo_url' => 'http://testmachine:3000/some-photo-path/3',
-                                        'audio_url' => 'http://testmachine:3000/some-audio-path/3',
-                                        'current_photo_key' => "photo-some-id-2", 'some_audio' => 'audio-some-id-2' )
-      child3.create_unique_id
+
+      before(:each) do
+        @user = User.new(
+            :user_name=>'user',
+            :full_name=>'name',
+            :organisation=>"UNICEF"
+        )
+
+        @child1 = Child.new_with_user_name(@user,
+          '_id' => '3-123451', 'name' => 'Dave', 'unique_identifier' => "xxxy",
+          'photo_url' => 'http://testmachine:3000/some-photo-path/1',
+          'audio_url' => 'http://testmachine:3000/some-audio-path/1',
+          'current_photo_key' => "photo-some-id-1", 'some_audio' => 'audio-some-id-1')
+        @child1.create_unique_id
+
+        @child2 = Child.new_with_user_name(@user,
+          '_id' => '4-153213', 'name' => 'Mary', 'unique_identifier' => "yyyx", 
+          'photo_url' => 'http://testmachine:3000/some-photo-path/2',
+          'audio_url' => 'http://testmachine:3000/some-audio-path/2',
+          'current_photo_key' => "photo-some-id-2", 'some_audio' => 'audio-some-id-2' )
+        @child2.create_unique_id
+
+        @child3 = Child.new_with_user_name(@user,
+                                          '_id' => '5-188888', 'name' => 'Jane', 'unique_identifier' => "yxyy",
+                                          'photo_url' => 'http://testmachine:3000/some-photo-path/3',
+                                          'audio_url' => 'http://testmachine:3000/some-audio-path/3',
+                                          'current_photo_key' => "photo-some-id-2", 'some_audio' => 'audio-some-id-2' )
+        @child3.create_unique_id
+      end
         
       subject do
-        ExportGenerator.new( [child1, child2, child3]).to_csv
+        ExportGenerator.new( [@child1, @child2, @child3]).to_csv
       end
       
       it 'should have a header for unique_identifier followed by all the user defined fields and metadata fields' do
@@ -78,9 +84,9 @@ describe ExportGenerator do
 
       it "should add metadata of children at the end" do
         csv_data = FasterCSV.parse subject.data
-        csv_data[1][4].should == 'user'
-        csv_data[1][5].should == 'UNICEF'
-        csv_data[1][4].should_not == ''
+        index = csv_data[0].index "Created by"
+        csv_data[1][index].should == 'user'
+        csv_data[1][index+1].should == 'UNICEF'
       end
 
       it "should not have updated_by info for child that was not edited" do
