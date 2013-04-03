a12c8d466868d47605b105ab1ef7fef696976e63
Faris Mohammed <farismosman@gmail.com>
2013-04-03 14:34:27 +0300

    [#1704] Faris : Removed features/support/hooks and features/support/paths

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -7,7 +7,7 @@
 
 require 'uri'
 require 'cgi'
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "../..","capybara_features", "support", "paths"))
 
 # Commonly used webrat steps
 # http://github.com/brynary/webrat
