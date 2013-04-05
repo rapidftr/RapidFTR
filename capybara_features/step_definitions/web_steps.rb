32eedcdd280dbcb1e6b8e34e5ea316a1bd00de13
Sanchari <ghoshroy.or.sanchari@gmail.com>
2013-04-05 17:18:21 +0530

    Fixed failing test for added scenarios in translations.feature

--- capybara_features/step_definitions/web_steps.rb
+++ capybara_features/step_definitions/web_steps.rb
@@ -35,7 +35,7 @@ When /^(?:|I )press "([^\"]*)"(?: within "([^\"]*)")?$/ do |button, selector|
 end
 
 When /^I search$/ do
-   page.execute_script("$('input[value=\"#{Search}\"]').click();")
+  page.execute_script("$('input[value=\"#{Search}\"]').click();")
 end
 
 When /^(?:|I )(?:can )?follow "([^\"]*)"(?: within "([^\"]*)")?$/ do |link, selector|
@@ -83,7 +83,7 @@ When /^(?:|I ) select "([^\"]*)" for "([^\"]*)"$/ do |value, field|
   page.execute_script "$('#{field}').trigger('focus')"
   page.execute_script "$('a.ui-datepicker-next').trigger('click')"
   page.execute_script "$(\"a.ui-state-default:contains('15')\").trigger(\"click\")"
-    # fill_in(field, :with => value)
+  # fill_in(field, :with => value)
 end
 
 # Use this to fill in an entire form with data from a table. Example:
@@ -142,7 +142,7 @@ Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
   else
     page.text.should match(regexp)
   end
-  end
+end
 
 Then /^(?:|I )should see "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, selector|
   with_scope(selector) do
