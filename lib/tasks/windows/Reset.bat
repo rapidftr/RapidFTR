0e18d0c09d01a1fb421650fb30a6e417ed7a0e4f
Subhas Dandapani <subhasda@thoughtworks.com>
2013-04-29 19:56:54 +0530

    1703 | Subhas | Fix windows installer rubygems issue

--- lib/tasks/windows/Reset.bat
+++ lib/tasks/windows/Reset.bat
@@ -1,8 +1,8 @@
 SET PATH=%PATH%;"%*"
 SET RAILS_ENV=standalone
 cd App
-gem install vendor/windows/ruby/1.8/cache/rubygems-update-1.8.25.gem --no-ri --no-rdoc
-gem install vendor/windows/ruby/1.8/cache/bundler-1.2.5.gem --no-ri --no-rdoc
-update_rubygems
-bundle install
-bundle exec rake db:create_couch_sysadmin couchdb:create db:seed db:migrate
+call gem install vendor/windows/ruby/1.8/cache/rubygems-update-1.8.25.gem --no-ri --no-rdoc
+call gem install vendor/windows/ruby/1.8/cache/bundler-1.2.5.gem --no-ri --no-rdoc
+call update_rubygems
+call bundle install
+bundle exec rake db:create_couch_sysadmin[rapidftr,rapidftr] couchdb:create db:seed db:migrate
