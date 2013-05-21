efd27bb29bc3c81b906e6224b4a1778c50190c30
Subhas Dandapani <subhasda@thoughtworks.com>
2013-05-21 15:02:46 +0530

    1806 | Subhas/Viky | Do not upgrade Rubygems/Bundler in windows installer

--- lib/tasks/windows/Reset.bat
+++ lib/tasks/windows/Reset.bat
@@ -1,8 +1,5 @@
 SET PATH=%PATH%;"%*"
 SET RAILS_ENV=standalone
 cd App
-call gem install --local vendor/bundle/ruby/1.8/cache/rubygems-update-1.8.25.gem --no-ri --no-rdoc
-call gem install --local vendor/bundle/ruby/1.8/cache/bundler-1.2.5.gem --no-ri --no-rdoc
-call update_rubygems
 call bundle install
 bundle exec rake db:create_couch_sysadmin[rapidftr,rapidftr] couchdb:create db:migrate db:seed
