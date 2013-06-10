1feaedfdd8c770b4a6938c4911a43ca901f803df
Yahya <yahyapo@thoughtworks.com>
2013-06-10 12:05:01 +0530

    #1844 - Yahya - Disable the Export button if no records present in RapidFTR

--- spec/views/children/_header.html.erb_spec.rb
+++ spec/views/children/_header.html.erb_spec.rb
@@ -39,9 +39,33 @@ describe "children/_header.html.erb" do
     end
   end
 
+  describe "export button" do
+    it "should show export button when there are children" do
+      @child = FactoryGirl.create(:child)
+      assign :children, @child
+      permissions =   [Permission::CHILDREN[:export_csv],
+                       Permission::CHILDREN[:view_and_search]]
+      @user.stub(:permissions => permissions)
+      render :partial => "children/header"
+      rendered.should have_tag("div[class='btn dropdown_btn']")
+    end
+
+    it "should not show export button when there are no children" do
+      permissions =   [Permission::CHILDREN[:export_csv],
+                       Permission::CHILDREN[:view_and_search]]
+      @user.stub(:permissions => permissions)
+      render :partial => "children/header"
+      rendered.should_not have_tag("div[class='btn dropdown_btn']")
+    end
+
+
+  end
+
   describe "export all records to CSV" do
     before :each do
       @url = children_path(:format => :csv)
+      @child = FactoryGirl.create(:child)
+      assign :children, @child
     end
 
     it_should_behave_like "not show links with per_page", []
@@ -52,6 +76,8 @@ describe "children/_header.html.erb" do
   describe "export all records to PDF" do
     before :each do
       @url = children_path(:format => :pdf)
+      @child = FactoryGirl.create(:child)
+      assign :children, @child
     end
 
     it_should_behave_like "not show links with per_page", []
