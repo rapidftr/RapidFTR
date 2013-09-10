1e29668cde75151797823d9236d373bd7304e2a3
Faris Mohammed <farismosman@gmail.com>
2013-09-10 16:22:48 +0300

    [#1861] Machaba/Faris | Refactored enquiries_controller#create to make it more readable

--- app/controllers/api/enquiries_controller.rb
+++ app/controllers/api/enquiries_controller.rb
@@ -4,38 +4,36 @@ class Api::EnquiriesController < Api::ApiController
 
   def create
     authorize! :create, Enquiry
-    object = params[:enquiry]
-    @enquiry = Enquiry.get(object[:id])
-    if !@enquiry.nil?
-      render :json => {:error => I18n.t("errors.models.enquiry.create_forbidden")}, :status => 403
-      return
-    end
-    @enquiry = Enquiry.new_with_user_name(current_user, object)
-    if !@enquiry.valid?
-      render :json => {:error => I18n.t("errors.models.enquiry.presence_of_criteria")}, :status => 422
-      return
-    else
-      @enquiry.save!
-      render :json => @enquiry, :status => 201
-    end
+
+    unless Enquiry.get(params['enquiry']['id']).nil? then render_error("errors.models.enquiry.create_forbidden", 403) and return end
+
+    @enquiry = Enquiry.new_with_user_name(current_user, params['enquiry'])
+
+    unless @enquiry.valid? then render_error("errors.models.enquiry.presence_of_criteria", 422) and return end
+
+    @enquiry.save!
+    render :json => @enquiry, :status => 201
   end
 
-  def update
-    authorize! :update, Enquiry
-    @enquiry = Enquiry.get(params[:enquiry][:id])
-    if @enquiry.nil?
-      render :json => {:error => "Not found"}, :status => 404
-    else
-      @enquiry.update_from(params[:enquiry])
-      @enquiry.save
-      render :json => @enquiry
+    def update
+      authorize! :update, Enquiry
+      @enquiry = Enquiry.get(params[:enquiry][:id])
+      if @enquiry.nil?
+        render :json => {:error => "Not found"}, :status => 404
+      else
+        @enquiry.update_from(params[:enquiry])
+        @enquiry.save
+        render :json => @enquiry
+      end
     end
-  end
 
   private
+
+    def render_error(error_message_key, status_code)
+      render :json => {:error => I18n.t(error_message_key)}, :status => status_code
+    end
 
     def sanitize_params
       super :enquiry
     end
-
 end
