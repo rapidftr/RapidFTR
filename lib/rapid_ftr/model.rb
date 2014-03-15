1d3dd138737957511f81063059c133ebb53c8274
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-03-14 19:04:59 -0600

    Task #4350 Upgrade RapidFTR : Deprecating couchrails_rest in favor of couchrest_model - Testing that the couchrest_model use the ActiveModel support and this provide the methods in this module, still testing...

--- lib/rapid_ftr/model.rb
+++ lib/rapid_ftr/model.rb
@@ -1,42 +1,42 @@
-require 'delegate'
-require 'active_model'
+#require 'delegate'
+#require 'active_model'
 
 module RapidFTR
   module Model
-    include ActiveModel::Conversion
-
-    def self.included(base)
-      base.extend ActiveModel::Naming
-    end
-
-    def persisted?
-      !new_record?
-    end
-
-    def _id=(new_id)
-      self["_id"] = new_id
-    end
-
-    def _id
-      self["_id"]
-    end
-
-    def errors
-      ErrorsAdapter.new super
-    end
-
-    def logger
-      Rails.logger
-    end
-
-    class ErrorsAdapter < SimpleDelegator
-      def [](key)
-        __getobj__[key] || []
-      end
-
-      def length
-        count
-      end
-    end
+#    include ActiveModel::Conversion
+#
+#    def self.included(base)
+#      base.extend ActiveModel::Naming
+#    end
+#
+#    def persisted?
+#      !new_record?
+#    end
+#
+#    def _id=(new_id)
+#      self["_id"] = new_id
+#    end
+#
+#    def _id
+#      self["_id"]
+#    end
+#
+#    def errors
+#      ErrorsAdapter.new super
+#    end
+#
+#    def logger
+#      Rails.logger
+#    end
+#
+#    class ErrorsAdapter < SimpleDelegator
+#      def [](key)
+#        __getobj__[key] || []
+#      end
+#
+#      def length
+#        count
+#      end
+#    end
   end
 end
