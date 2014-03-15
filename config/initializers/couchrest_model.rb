41d2b3474ea74e01aa480cd1bc8b7aa5ab7c41e3
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-03-14 19:03:39 -0600

    Minor change, using default value.

--- config/initializers/couchrest_model.rb
+++ config/initializers/couchrest_model.rb
@@ -1,6 +1,5 @@
 #Configure for compatibility with older version.
 #Current model_type_key is 'type', older is 'couchrest-type'
 CouchRest::Model::Base.configure do |config|
-  config.mass_assign_any_attribute = true
   config.model_type_key = 'couchrest-type'
 end
