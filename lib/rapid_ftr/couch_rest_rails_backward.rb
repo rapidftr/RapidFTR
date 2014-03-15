c48bcb95dc523576583f0c9a907743424e372ade
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-03-14 19:01:23 -0600

    Task #4350 Upgrade RapidFTR : Deprecating couchrails_rest in favor of couchrest_model - Refatoring view method, some index like the user controller call the view method expecting to get the models instead the couchdb docs, so added view_raw to get the object model instead the couchdb doc.

--- lib/rapid_ftr/couch_rest_rails_backward.rb
+++ lib/rapid_ftr/couch_rest_rails_backward.rb
@@ -9,10 +9,28 @@ module RapidFTR
     end
 
     module ClassMethods
+
       def paginate(pagination_options = {})
         paginates_per pagination_options[:per_page] if pagination_options[:per_page]
         design_view = eval("self.#{pagination_options[:view_name]}(#{pagination_options}).page(#{pagination_options[:page]})")
         #We need to returns the model object
+        convert_to_model(design_view)
+      end
+
+      #This method returns a CouchRest::Model::Designs::View
+      #If we want the object model, use view_raw.
+      def view(view_name, options = {})
+        send(view_name, options)
+      end
+
+      #This method returns the object model instead the view.
+      def view_raw(view_name, options = {})
+        convert_to_model(send(view_name, options))
+      end
+
+      #This method convert the docs in the design view
+      #to the object model.
+      def convert_to_model(design_view)
         result = []
         design_view.each do |model_obj|
           result << model_obj
@@ -20,10 +38,6 @@ module RapidFTR
         result
       end
 
-      def view(view_name, options = {})
-        send(view_name, options)
-      end
     end
-
   end
 end
