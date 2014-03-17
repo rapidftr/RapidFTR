f0a96c5b743de7ee06d8e399e3ee058182b27d77
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-03-16 21:44:55 -0600

    Task #4350 Upgrade RapidFTR : Deprecating couchrails_rest in favor of couchrest_model - Allow create dynamic attributes, for example child entity has a lot of attributes in the form but there are not declared as properties.

--- config/initializers/couchrest_model.rb
+++ config/initializers/couchrest_model.rb
@@ -1,5 +1,6 @@
 #Configure for compatibility with older version.
 #Current model_type_key is 'type', older is 'couchrest-type'
 CouchRest::Model::Base.configure do |config|
+  config.mass_assign_any_attribute = true
   config.model_type_key = 'couchrest-type'
 end
