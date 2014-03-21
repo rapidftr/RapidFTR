9bff1585b80daeaa13f5bd9c9c3f581952250db5
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-03-21 08:19:36 -0600

    Task #4350 Upgrade RapidFTR : Deprecating couchrails_rest in favor of couchrest_model - Fixed Rspec test.

--- spec/controllers/user_histories_controller_spec.rb
+++ spec/controllers/user_histories_controller_spec.rb
@@ -22,13 +22,13 @@ describe UserHistoriesController do
 
     get :index, :id => "1"
 
-    assigns(:histories).should == [history1.merge("child_id" => "another_id", "child_name" => "another_name"), history2.merge("child_id" => "other_id", "child_name" => "other_name")]
+    assigns(:histories).should == [history1.merge(:child_id => "another_id", :child_name => "another_name"), history2.merge(:child_id => "other_id", :child_name => "other_name")]
   end
 
   it "should remove history entries for other users" do
     User.should_receive(:get).with("1").and_return(double(:user_name => (user_name = "some user")))
     history1 = {"user_name" => "some user", "datetime" => 1.day.ago.to_s}
-    history2 = {"user_name" => "some other user", "datetime" => 1.week.ago.to_s}    
+    history2 = {"user_name" => "some other user", "datetime" => 1.week.ago.to_s}
     Child.should_receive(:all_connected_with).with(user_name).and_return([
           double(:id => "other_id", :name => "other_name", :histories => [history2]),
           double(:id => "another_id", :name => "another_name", :histories => [history1])
@@ -36,7 +36,7 @@ describe UserHistoriesController do
 
     get :index, :id => "1"
 
-    assigns(:histories).should == [history1.merge("child_id" => "another_id", "child_name" => "another_name")]
+    assigns(:histories).should == [history1.merge(:child_id => "another_id", :child_name => "another_name")]
   end
 
   it "should set the page name to the user" do
