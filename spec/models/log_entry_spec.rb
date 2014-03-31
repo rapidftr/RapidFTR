ebde1d10165e56de602924af034abe88523e8d9a
Chavez, Carlos <carlos.chavez@quoininc.com>
2014-03-31 17:15:04 -0600

    Returns docs from the method 'all' of CouchRest::Model::Designs::View.

--- spec/models/log_entry_spec.rb
+++ spec/models/log_entry_spec.rb
@@ -3,6 +3,8 @@ require "spec_helper"
 describe LogEntry do
 
 	it "should save with creation timestamp" do
+require 'pry'
+binding.pry
 		RapidFTR::Clock.stub(:current_formatted_time).and_return("this is now")
 
 		log_entry = LogEntry.create! :type => LogEntry::TYPE[:cpims], :username => "rapidftr", :organisation => "urc", :format => "cpims", :number_or_records => "123"
@@ -19,7 +21,7 @@ describe LogEntry do
 		Clock.stub(:now).and_return(2.days.ago)
 		oldest_entry = LogEntry.create! :type => LogEntry::TYPE[:pdf], :username => "rapidftr", :organisation => "urc", :format => "cpims", :number_or_records => "123"
 
-		entries = LogEntry.by_created_at(:descending => true)
+		entries = LogEntry.by_created_at(:descending => true).all
 		entries.size.should == 3
 		entries[0][:type].should == LogEntry::TYPE[:cpims]
 		entries[1][:type].should == LogEntry::TYPE[:csv]
