ff3f279ada09ffd54ad26e1e258bd91bf6fdf4ca
Joshua Toliver <jtoliver@quoininc.com>
2014-04-04 14:50:46 -0400

    Rspec 2.14 changes the way you use rspec_reset. Was moved to RSpec::Mocks

--- capybara_features/step_definitions/replication_steps.rb
+++ capybara_features/step_definitions/replication_steps.rb
@@ -1,15 +1,15 @@
 When /^I make sure that the Replication Configuration request succeeds$/ do
-  Net::HTTP.rspec_reset
+  RSpec::Mocks.proxy_for(Net::HTTP).reset
   Net::HTTP.should_receive(:post_form).any_number_of_times do |*args|
     double :body => { "target" => "http://localhost:1234", "databases" => {} }.to_json
   end
 end
 
 When /^I make sure that the Replication Configuration request fails$/ do
-  Net::HTTP.rspec_reset
+  RSpec::Mocks.proxy_for(Net::HTTP).reset
   Net::HTTP.should_receive(:post_form).any_number_of_times.and_raise("some http error")
 end
 
 When /^I clear the Replication Configuration expectations$/ do
-  Net::HTTP.rspec_reset
+  RSpec::Mocks.proxy_for(Net::HTTP).reset
 end
