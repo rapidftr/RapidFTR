114590cbbe91f20c34f8bc417881dc217ab3d77e
Mark <zoodor+github@gmail.com>
2014-05-13 21:49:52 +0100

    [#1814] Mark - Remove test that doesn't work

--- capybara_features/step_definitions/child_record_toolbar_steps.rb
+++ capybara_features/step_definitions/child_record_toolbar_steps.rb
@@ -1,12 +1,3 @@
-Then /^I should see the following links in the toolbars:$/ do |links_table|
-  divs = page.all :xpath, '//div[@class="profile-tools"]'
-  divs.each do |div|
-    links_table.hashes.each do |link_hash|
-      check_link_presence(div, link_hash['link_class_name'], link_hash['link_text'])
-    end
-  end
-end
-
 And /^I mark as investigated with the following details:$/ do |details|
   child_record_toolbar.mark_as_investigated(details)
 end
