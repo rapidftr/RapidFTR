1f80c58561000cbaaefad9666433facf6aa5299d
Mark <zoodor+github@gmail.com>
2014-05-13 20:20:34 +0100

    [#1814] Mark - Swap double for single quotes
    In cases where no string interpolation is occurring.

--- capybara_features/support/pages/child_record_toolbar_widget.rb
+++ capybara_features/support/pages/child_record_toolbar_widget.rb
@@ -9,15 +9,15 @@ class ChildRecordToolbarWidget
   end
 
   def mark_as_investigated(details)
-    @session.click_link("Mark as Investigated")
-    @session.fill_in("Investigation Details", :with => details)
-    @session.click_button("Mark as Investigated")
+    @session.click_link('Mark as Investigated')
+    @session.fill_in('Investigation Details', :with => details)
+    @session.click_button('Mark as Investigated')
   end
 
   def mark_as_not_investigated(details)
-    @session.click_link("Mark as Not Investigated")
-    @session.fill_in("Undo Investigation Details", :with => details)
-    @session.click_button("Undo Investigated")
+    @session.click_link('Mark as Not Investigated')
+    @session.fill_in('Undo Investigation Details', :with => details)
+    @session.click_button('Undo Investigated')
   end
 
 end
