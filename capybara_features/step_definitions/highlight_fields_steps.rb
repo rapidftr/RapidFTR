1ff7f2f2d8508fe1b09656023822b5e3245fbdce
Mark <zoodor+github@gmail.com>
2014-05-19 20:49:56 +0100

    [#1814] Mark - Move step to more logical place
    The step was for a different page to the steps file it had been placed
    in.

--- capybara_features/step_definitions/highlight_fields_steps.rb
+++ capybara_features/step_definitions/highlight_fields_steps.rb
@@ -2,10 +2,6 @@ Given /^I select menu "(.+)"$/ do |text_value|
   page.find('//li', :text => text_value).click
 end
 
-And /^I select dropdown option "(.+)"$/ do |option|
-  page.find('//option', :text => option).click
-end
-
 And /^I remove highlight "(.+)"$/ do |highlight_field|
   page.find('//td', :text => highlight_field).find('..').click_link('remove')
 end
