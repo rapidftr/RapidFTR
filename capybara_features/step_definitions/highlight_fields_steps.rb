4defd0fa3e907f0a3eadf372d6defdd843c4082b
Mark <zoodor+github@gmail.com>
2014-05-19 21:06:04 +0100

    [#1814] Mark - Move code into highlight_fields_page

--- capybara_features/step_definitions/highlight_fields_steps.rb
+++ capybara_features/step_definitions/highlight_fields_steps.rb
@@ -1,9 +1,13 @@
 Given /^I select menu "(.+)"$/ do |text_value|
-  page.find('//li', :text => text_value).click
+  highlight_fields_page.select_menu_with_text(text_value)
 end
 
-And /^I remove highlight "(.+)"$/ do |highlight_field|
-  page.find('//td', :text => highlight_field).find('..').click_link('remove')
+And /^I remove highlight "(.+)"$/ do |field_name|
+  highlight_fields_page.remove_field_with_name(field_name)
 end
 
+private
 
+def highlight_fields_page
+  HighlightFieldsPage.new(Capybara.current_session)
+end
