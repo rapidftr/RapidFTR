e9a619039bd57a9bea4526d8ec937d67d461df91
Subhas Dandapani <rdsubhas@gmail.com>
2014-05-23 17:22:39 +0300

    Remove chef folder, moved to rapidftr-infrastructure repository

--- chef/cookbooks/passenger/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'passenger'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures passenger'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
