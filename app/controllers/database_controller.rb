ad8c0dbf03c3a0c3a295e74cde4776e173451043
Tom Oketch <tc.oketch@gmail.com>
2014-06-13 19:17:18 +0300

    Refactor call to destroy all data in Test Environments

--- app/controllers/database_controller.rb
+++ app/controllers/database_controller.rb
@@ -1,15 +1,17 @@
 class DatabaseController < ApplicationController
 
-  def delete_children
-    if Rails.env.android?
-      Child.all.each do |child|
-        child.destroy
-      end
-      render :text => t("data_base.delete_all_documents")
-    else
-      render :text => t("data_base.operation_not_allowed", :rails_env => Rails.env)
-    end
+  def delete_data
+    raise "Database operation not allowed" unless (Rails.env.android? || Rails.env.test? || Rails.env.development? || Rails.env.cucumber?)
 
+    data_type   = params[:data_type]
+    model_class = data_type.camelize.constantize
+
+    docs = model_class.database.documents["rows"].map { |doc|
+      { "_id" => doc["id"], "_rev" => doc["value"]["rev"], "_deleted" => true } unless doc["id"].include? "_design"
+    }.compact
+    RestClient.post "#{model_class.database.root}/_bulk_docs", { :docs => docs }.to_json, { "Content-type" => "application/json" } unless docs.empty?
+
+    render text: "Deleted all #{data_type} documents"
   end
 
 end
--- spec/controllers/database_controller_spec.rb
+++ spec/controllers/database_controller_spec.rb
@@ -14,13 +14,28 @@ describe DatabaseController do
     Rails.env = @original_rails_env
   end
 
-  it "should delete all children in android environment" do
+  it "should delete all child models in non-production environments" do
     User.stub!(:find_by_user_name).with("me").and_return(mock(:organisation => "stc"))
     Child.create('last_known_location' => "London", :created_by => "me")
     Child.create('last_known_location' => "India", :created_by => "me")
     
-    delete :delete_children
+    delete :delete_data, :data_type => "child"
     
     Child.all.should be_empty
   end
+
+  it "should delete all enquiry models in non-production environments" do
+    User.stub!(:find_by_user_name).with("me").and_return(mock(:organisation => "stc"))
+    Enquiry.create({:enquirer_name => 'Someone', :criteria => {'name' => 'child name'}})
+    Enquiry.create({:enquirer_name => 'Someone Else', :criteria => {'name' => 'child name'}})
+    
+    delete :delete_data, :data_type => "enquiry"
+    
+    Enquiry.all.should be_empty
+  end
+
+  it "should not delete any models in production environments" do
+    Rails.env = "production"
+    expect { delete :delete_data, :data_type => "enquiry" }.to raise_error
+  end  
 end
