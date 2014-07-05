7c7c9c9eb39747fbd80b98aa80bbed87494a946d
rdsubhas <rdsubhas@gmail.com>
2014-07-06 01:06:06 +0300

    Added initial working Docker configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,22 +2,21 @@ RapidFTR::Application.configure do
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
-  config.eager_load = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # See everything in the log (default is :info)
+  config.log_level = :error
 
-  # Disable Rails's static asset server
-  # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
-
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
 
   # Asset pipeline
-  config.assets.compile = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+  config.assets.compile = false
+  config.assets.digest = true
+  config.assets.compress = true
 end
