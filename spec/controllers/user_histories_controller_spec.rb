359f7bc193d2324d7f8d4ad855317f45576267be
rdsubhas <rdsubhas@gmail.com>
2014-07-08 09:43:21 +0300

    #40 @andreweskeclarke @rdsubhas Fix hardcoded names in headers, made Duplicate UI to match usual summary layout

--- spec/controllers/user_histories_controller_spec.rb
+++ spec/controllers/user_histories_controller_spec.rb
@@ -11,7 +11,7 @@ describe UserHistoriesController, :type => :controller do
                     {:controller => "user_histories", :action => "index", :id => "1"})
   end
 
-  it "should pass ordered histories, child name and id for user to view" do
+  it "should pass ordered histories and child id for user to view" do
     history1 = {"user_name" => "some user", "datetime" => 1.day.ago.to_s}
     history2 = {"user_name" => "some user", "datetime" => 1.week.ago.to_s}
     expect(User).to receive(:get).with("1").and_return(double(:user_name => (user_name = "some user")))
@@ -22,7 +22,7 @@ describe UserHistoriesController, :type => :controller do
 
     get :index, :id => "1"
 
-    expect(assigns(:histories)).to eq([history1.merge(:child_id => "another_id", :child_name => "another_name"), history2.merge(:child_id => "other_id", :child_name => "other_name")])
+    expect(assigns(:histories)).to eq([history1.merge(:child_id => "another_id"), history2.merge(:child_id => "other_id")])
   end
 
   it "should remove history entries for other users" do
@@ -36,7 +36,7 @@ describe UserHistoriesController, :type => :controller do
 
     get :index, :id => "1"
 
-    expect(assigns(:histories)).to eq([history1.merge(:child_id => "another_id", :child_name => "another_name")])
+    expect(assigns(:histories)).to eq([history1.merge(:child_id => "another_id")])
   end
 
   it "should set the page name to the user" do
