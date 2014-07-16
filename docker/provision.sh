228d38f85b7523c161397bb76296057c1e6b4a47
rdsubhas <rdsubhas@gmail.com>
2014-07-16 17:47:15 +0300

    Fix some weird Docker issues with PAM on host

--- docker/provision.sh
+++ docker/provision.sh
@@ -3,6 +3,13 @@ set -e
 source /build/buildconfig
 set -x
 
+# PAM conflicts with users on Host machine!
+#   https://github.com/dotcloud/docker/issues/6345#issuecomment-46791228
+#   This workaround is required temporarily, can be removed after
+#   Docker Hub kernel is upgraded or issue is fixed
+export DEBIAN_FRONTEND=noninteractive
+alias adduser='useradd'
+
 rm -rf /var/lib/apt/lists/*
 apt-get update
 $minimal_apt_get_install build-essential git libxml2-dev libxslt1-dev zlib1g-dev imagemagick python-software-properties openjdk-7-jre-headless uuid-runtime couchdb uuid-runtime
