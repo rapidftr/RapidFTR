25cd759b8e4acde4a6cd5e1e2933b3a6680ae93d
rdsubhas <rdsubhas@gmail.com>
2014-07-21 17:47:03 +0300

    Sanity check in image migration rapidftr/tracker#106

--- db/migration/0012_base64_decode_images.rb
+++ db/migration/0012_base64_decode_images.rb
@@ -1,45 +1,46 @@
-require 'progress_bar'
-require 'base64'
-
 rows = Child.database.all_docs['rows']
-return unless rows.count > 0
-progressbar = ProgressBar.new rows.count
-errors = []
+if rows.count > 0
+  require 'progress_bar'
+  require 'base64'
 
-rows.each do |row|
-  begin
-    doc = Child.database.get row['id']
-    attachments = doc['_attachments']
-    attachments.each do |attachment_id, attachment_meta|
-      if attachment_meta['content_type'].start_with? 'image/'
-        data = Child.database.fetch_attachment doc, attachment_id
-        begin
-          MiniMagick::Image.read data
-        rescue MiniMagick::Invalid
-          data64 = Base64.decode64 data
-          MiniMagick::Image.read data64
-          Child.database.put_attachment doc, attachment_id, data64
-          doc = Child.database.get row['id']
+  progressbar = ProgressBar.new rows.count
+  errors = []
+
+  rows.each do |row|
+    begin
+      doc = Child.database.get row['id']
+      attachments = doc['_attachments']
+      attachments.each do |attachment_id, attachment_meta|
+        if attachment_meta['content_type'].start_with? 'image/'
+          data = Child.database.fetch_attachment doc, attachment_id
+          begin
+            MiniMagick::Image.read data
+          rescue MiniMagick::Invalid
+            data64 = Base64.decode64 data
+            MiniMagick::Image.read data64
+            Child.database.put_attachment doc, attachment_id, data64
+            doc = Child.database.get row['id']
+          end
         end
+      end if doc['_attachments']
+    rescue => e
+      if errors.count == 0
+        puts
+        puts "Logging the first error, further errors will be suppressed"
+        puts e.message
+        puts e.backtrace
       end
-    end if doc['_attachments']
-  rescue => e
-    if errors.count == 0
-      puts
-      puts "Logging the first error, further errors will be suppressed"
-      puts e.message
-      puts e.backtrace
+
+      errors << row['id']
     end
 
-    errors << row['id']
+    progressbar.increment!
   end
 
-  progressbar.increment!
-end
-
-if errors.count > 0
-  puts
-  puts "Failed to migrate images for following records:"
-  puts errors
-  raise "Migration failed" unless ENV['IGNORE_0012'] == 'true'
+  if errors.count > 0
+    puts
+    puts "Failed to migrate images for following records:"
+    puts errors
+    raise "Migration failed" unless ENV['IGNORE_0012'] == 'true'
+  end
 end
