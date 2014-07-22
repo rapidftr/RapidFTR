077b26ee2d0d4ffaea5aba04eff905268afdd45b
rdsubhas <rdsubhas@gmail.com>
2014-07-22 23:17:54 +0300

    Minor Docker refactorings

--- docker/boot/production.sh
+++ docker/boot/production.sh
@@ -2,9 +2,6 @@
 # Run upon first boot of RapidFTR container
 # This service is complicated, because of many circular dependencies, need to refactor
 
-mkdir -p /data/couchdb
-chown -R couchdb:couchdb /data/couchdb
-
 cd /rapidftr
 if [ ! -f config/couchdb.yml ]; then
   #Generate random username/password for CouchDB
@@ -18,7 +15,6 @@ fi
 bundle exec rake assets:clobber assets:precompile
 
 # Seed database
-chown -R www-data:www-data .
 runsv /etc/service/couchdb &
 runsv /etc/service/solr &
 sleep 5
@@ -28,23 +24,23 @@ bundle exec rake sunspot:reindex
 
 sv shutdown couchdb
 sv shutdown solr
-killall java
 sleep 5
+killall java
 
 # Generate a self-signed SSL certificate if none found
 mkdir -p /data/ssl
 if [ ! -f /data/ssl/certificate.crt ]; then
   echo "********************************************"
   echo "********************************************"
-  echo "***  WARNING:  No SSL certificate found  ***"
-  echo "*** Generating a self-signed certificate ***"
-  echo "***    Please use a proper certificate   ***"
+  echo "***  WARNING:  NO SSL CERTIFICATE FOUND  ***"
+  echo "*** GENERATING A SELF-SIGNED CERTIFICATE ***"
+  echo "***    PLEASE USE A PROPER CERTIFICATE   ***"
   echo "********************************************"
   echo "********************************************"
-  openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=UG/ST=Kampala/L=Kampala/O=RapidFTRDemo/CN=localhost" -keyout /data/ssl/certificate.key -out /data/ssl/certificate.crt
+  openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=FIXME/ST=FIXME/L=FIXME/O=FIXME/CN=FIXME" -keyout /data/ssl/certificate.key -out /data/ssl/certificate.crt
 fi
 chown -R root:root /data/ssl
 chmod 0600 /data/ssl
 
-# Make sure permissions are unchanged
-chown -R www-data:www-data .
+# Remove this file, it is no longer needed
+rm $0
