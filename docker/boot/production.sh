8f54e9e93ea9f97c72f58c090d095cd3212a53c9
rdsubhas <rdsubhas@gmail.com>
2014-07-23 12:27:05 +0300

    Docker: Enable HTTPS in CouchDB

--- docker/boot/production.sh
+++ docker/boot/production.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 # Run upon first boot of RapidFTR container
-# This service is complicated, because of many circular dependencies, need to refactor
+# This service is complicated because of many circular dependencies with runit
 
+# Generate a self-signed SSL certificate if none found
+mkdir -p /data/ssl
+if [ ! -f /data/ssl/certificate.crt ]; then
+  echo "********************************************"
+  echo "********************************************"
+  echo "***  WARNING:  NO SSL CERTIFICATE FOUND  ***"
+  echo "*** GENERATING A SELF-SIGNED CERTIFICATE ***"
+  echo "***    PLEASE USE A PROPER CERTIFICATE   ***"
+  echo "********************************************"
+  echo "********************************************"
+  openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=XX/ST=FIXME/L=FIXME/O=FIXME/CN=FIXME" -keyout /data/ssl/certificate.key -out /data/ssl/certificate.crt
+fi
+chown -R root:root /data/ssl
+chmod 0700 /data/ssl
+chmod 0600 /data/ssl/*
+
+# Generate random username/password for CouchDB
 cd /rapidftr
 if [ ! -f config/couchdb.yml ]; then
-  #Generate random username/password for CouchDB
   password=$(uuidgen | tr -d '-')
   username=$(uuidgen | tr -d '-')
   echo -e "[admins]\n$username = $password" > /etc/couchdb/local.d/rapidftr-security.ini
@@ -26,21 +42,3 @@ sv shutdown couchdb
 sv shutdown solr
 sleep 5
 killall java
-
-# Generate a self-signed SSL certificate if none found
-mkdir -p /data/ssl
-if [ ! -f /data/ssl/certificate.crt ]; then
-  echo "********************************************"
-  echo "********************************************"
-  echo "***  WARNING:  NO SSL CERTIFICATE FOUND  ***"
-  echo "*** GENERATING A SELF-SIGNED CERTIFICATE ***"
-  echo "***    PLEASE USE A PROPER CERTIFICATE   ***"
-  echo "********************************************"
-  echo "********************************************"
-  openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=FIXME/ST=FIXME/L=FIXME/O=FIXME/CN=FIXME" -keyout /data/ssl/certificate.key -out /data/ssl/certificate.crt
-fi
-chown -R root:root /data/ssl
-chmod 0600 /data/ssl
-
-# Remove this file, it is no longer needed
-rm $0
