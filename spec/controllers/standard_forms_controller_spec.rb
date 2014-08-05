a4684bedfb36318f3af670b2414fc44fb5b37524
andreweskeclarke <andreweskeclarke@gmail.com>
2014-08-05 17:32:47 +0300

    rapidftr/tracker#48 @andreweskeclarke @austiine04 Marking saved standard forms and formsections as disabled for future use in the UI

--- spec/controllers/standard_forms_controller_spec.rb
+++ spec/controllers/standard_forms_controller_spec.rb
@@ -15,12 +15,72 @@ describe StandardFormsController, :type => :controller do
       enquiry_form_sections = [FormSection.new(:name => 'Enquiry', :fields => [field2])]
       allow(RapidFTR::ChildrenFormSectionSetup).to receive(:build_form_sections).and_return(child_form_sections)
       allow(RapidFTR::EnquiriesFormSectionSetup).to receive(:build_form_sections).and_return(enquiry_form_sections)
-     
       get :index
-     
-      expect(assigns[:forms]).to eq({child: child_form_sections, enquiry: enquiry_form_sections })
+      expect(assigns[:forms]).to include(child: { disabled: false, sections: child_form_sections})
+      expect(assigns[:forms]).to include(enquiry: { disabled: false, sections: enquiry_form_sections})
     end
 
-  end
+    describe "disabling form checkboxes" do
+      it "should not disable forms by default" do
+        get :index
+        forms = assigns[:forms]
+        expect(forms[:child][:disabled]).to be(false)
+        expect(forms[:child][:disabled]).to be(false)
+      end
 
+      it "should disable already existing enquiry form" do
+        create :form, name: Enquiry::FORM_NAME
+        get :index
+        forms = assigns[:forms]
+        expect(forms[:enquiry][:disabled]).to be(true)
+      end
+
+      it "should disable already existing child form" do
+        create :form, name: Child::FORM_NAME
+        get :index
+        forms = assigns[:forms]
+        expect(forms[:child][:disabled]).to be(true)
+      end
+    end
+
+    describe "disabling form section checkboxes" do
+      it "should not disable child form sections by default" do
+        form_section = build :form_section, name: "Basic Identity"
+        allow(RapidFTR::ChildrenFormSectionSetup).to receive(:build_form_sections).and_return([form_section])
+        get :index
+        child_form_sections = assigns[:forms][:child][:sections]
+        expect(child_form_sections.first[:disabled]).to be_falsey
+      end
+
+      it "should not disable enquiry form sections by default" do
+        form_section = build :form_section, name: "Enquiry Criteria"
+        allow(RapidFTR::EnquiriesFormSectionSetup).to receive(:build_form_sections).and_return([form_section])
+        get :index
+        enquiry_form_sections = assigns[:forms][:enquiry][:sections]
+        expect(enquiry_form_sections.first[:disabled]).to be_falsey
+      end
+
+      it "should disable already existing enquiry form sections" do
+        form = create :form, name: Enquiry::FORM_NAME
+        form_section = RapidFTR::EnquiriesFormSectionSetup.build_form_sections.first
+        form_section.form = form
+        form_section.save
+
+        get :index
+        enquiry_form_sections = assigns[:forms][:enquiry][:sections]
+        expect(enquiry_form_sections.first[:disabled]).to be(true)
+      end
+
+      it "should disable already existing child form sections" do
+        form = create :form, name: Child::FORM_NAME
+        form_section = RapidFTR::ChildrenFormSectionSetup.build_form_sections.first
+        form_section.form = form
+        form_section.save
+
+        get :index
+        child_form_sections = assigns[:forms][:child][:sections]
+        expect(child_form_sections.first[:disabled]).to be(true)
+      end
+    end
+  end
 end
