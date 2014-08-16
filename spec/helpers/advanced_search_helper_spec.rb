b6b0e097cc2e7e4a0eab92931fc6285bce7c7aa8
Erik Michaels-Ober <sferik@gmail.com>
2014-08-16 05:00:27 +0200

    Use % instead of %Q

--- spec/helpers/advanced_search_helper_spec.rb
+++ spec/helpers/advanced_search_helper_spec.rb
@@ -4,7 +4,7 @@ describe AdvancedSearchHelper, :type => :helper do
   it "should generate the appropriate html for given text field criteria" do
     field = Field.new(:name => "name", :display_name => "Name", :type => Field::TEXT_FIELD)
     criteria = {:join => "", :display_name => "Name", :index => "0", :field => "name", :value => "test"}
-    expect(helper.generate_html(criteria, [field]).gsub("\n", '')).to eq(%Q{<p class='criterion-selected'><a class="select-criteria">Name</a>
+    expect(helper.generate_html(criteria, [field]).gsub("\n", '')).to eq(%{<p class='criterion-selected'><a class="select-criteria">Name</a>
 <input class="criteria-field" type="hidden" value="name" name="criteria_list[0][field]">
 <input class="criteria-index" type="hidden" value="0" name="criteria_list[0][index]">
 <span class=\"criteria-values\"/>
@@ -15,7 +15,7 @@ describe AdvancedSearchHelper, :type => :helper do
   it "should generate the appropriate html for given select box criteria" do
     field = Field.new(:name => "protection_status", :display_name => "Protection Status", :type => Field::SELECT_BOX, :option_strings_text => "\nUnaccompanied\nSeparated")
     criteria = {:join => "AND", :display_name => "Protection Status", :index => "1", :field => "protection_status", :value => "Separated"}
-    expect(helper.generate_html(criteria, [field]).gsub("\n", '')).to eq(%Q{<p class='criterion-selected'>
+    expect(helper.generate_html(criteria, [field]).gsub("\n", '')).to eq(%{<p class='criterion-selected'>
 <input id="criteria_join_and" type="radio" value="AND" checked='' name="criteria_list[1][join]">
 <label for="criteria_join_and">And</label>
 <input id="criteria_join_or" type="radio" value="OR"  name="criteria_list[1][join]">
