0d75c0427e9b3954d3e3a37b0d2ca110a61ad440
andreweskeclarke <andreweskeclarke@gmail.com>
2014-09-02 13:06:46 +0300

    rapidftr/tracker#187 @andreweskeclarke @mwangiann Fixing broken browser test

--- features/step_definitions/form_field_steps.rb
+++ features/step_definitions/form_field_steps.rb
@@ -18,7 +18,7 @@ end
 
 Then /^(?:|I )move field "([^"]*)" to form "([^"]*)"$/ do |field_name, form_name|
   # //td[text()='#{field_name}']/parent::tr/td[6]/a
-  page.find("//tr[@data='#{field_name}']/td[6]/a").click
+  page.find("//tr[@data='#{field_name}']/td[7]/a").click
   sleep 1.to_i
   page.select(form_name, :from => "#{field_name}_destination_form_id")
 end
