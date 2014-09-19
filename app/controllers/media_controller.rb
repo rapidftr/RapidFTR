2fbb4cfdb23560bd628871ac07c39c76e89bad42
Kisitu Augustine <austiine04@gmail.com>
2014-09-19 17:32:26 +0300

    rapidftr/tracker#174 @austiine04 @mwangiann generalized media_controller to reuse it for both enquiry and child media

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,8 +1,7 @@
 class MediaController < ApplicationController
   NO_PHOTO_FORMAT = 'jpg'
   NO_PHOTO_CLIP = File.binread(Rails.root.join("app/assets/images/no_photo_clip.#{NO_PHOTO_FORMAT}"))
-  helper :children
-  before_action :find_child
+  before_action :find_model
   before_action :find_photo_attachment, :only => [:show_photo, :show_resized_photo, :show_thumbnail]
 
   def index
@@ -25,7 +24,8 @@ class MediaController < ApplicationController
 
   def download_audio
     find_audio_attachment
-    redirect_to(:controller => 'children', :action => 'show', :id => @child.id) && return unless @attachment
+    controller = get_controller_name_from_current_uri
+    redirect_to(:controller => controller, :action => 'show', :id => @model.id) && return unless @attachment
     send_data(@attachment.data.read, :filename => audio_filename(@attachment), :type => @attachment.content_type)
   end
 
@@ -35,22 +35,24 @@ class MediaController < ApplicationController
 
   private
 
-  def find_child
-    @child = Child.get(params[:child_id])
+  def find_model
+    model_class = get_model_name_from_current_uri
+    model_id = "#{model_class}_id".downcase.to_sym
+    @model = model_class.get(params[model_id])
   end
 
   def find_audio_attachment
-    @attachment = params[:id] ? @child.media_for_key(params[:id]) : @child.audio
+    @attachment = params[:id] ? @model.media_for_key(params[:id]) : @model.audio
   rescue => e
     logger.error(e.inspect)
   end
 
   def find_photo_attachment
-    redirect_to(:photo_id => @child.current_photo_key, :ts => @child.last_updated_at) && return if
-      params[:photo_id].to_s.empty? && @child.current_photo_key.present?
+    redirect_to(:photo_id => @model.current_photo_key, :ts => @model.last_updated_at) && return if
+      params[:photo_id].to_s.empty? && @model.current_photo_key.present?
 
     begin
-      @attachment = params[:photo_id] == '_missing_' ? no_photo_attachment : @child.media_for_key(params[:photo_id])
+      @attachment = params[:photo_id] == '_missing_' ? no_photo_attachment : @model.media_for_key(params[:photo_id])
     rescue => e
       logger.warn 'Error getting photo'
       logger.warn e.inspect
@@ -64,15 +66,15 @@ class MediaController < ApplicationController
   end
 
   def audio_filename(attachment)
-    'audio_' + @child.unique_identifier + AudioMimeTypes.to_file_extension(attachment.mime_type)
+    'audio_' + @model.unique_identifier + AudioMimeTypes.to_file_extension(attachment.mime_type)
   end
 
   def photos_details
-    @child['photo_keys'].map do |photo_key|
+    @model['photo_keys'].map do |photo_key|
       {
-        :photo_url => child_photo_url(@child, photo_key),
-        :thumbnail_url => child_thumbnail_url(@child, photo_key),
-        :select_primary_photo_url => child_select_primary_photo_url(@child, photo_key)
+        :photo_url => child_photo_url(@model, photo_key),
+        :thumbnail_url => child_thumbnail_url(@model, photo_key),
+        :select_primary_photo_url => child_select_primary_photo_url(@model, photo_key)
       }
     end
   end
@@ -80,5 +82,14 @@ class MediaController < ApplicationController
   def send_photo_data(*args)
     expires_in 1.year, :public => true if params[:ts]
     send_data(*args)
+  end
+
+  def get_model_name_from_current_uri
+    url_for(:only_path => true).split("/")[1].singularize.capitalize.constantize
+  end
+
+  def get_controller_name_from_current_uri
+    model_name = get_model_name_from_current_uri.to_s
+    model_name.downcase.pluralize
   end
 end
--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -6,11 +6,12 @@ describe MediaController, :type => :controller do
   include CustomMatchers
   include MiniMagickConversions
 
-  before do
-    fake_login
-  end
-
   shared_examples 'photos' do
+    before do
+      fake_login
+      @model_id = "#{model}_id".to_sym
+    end
+
     describe '#send_photo_data' do
       it 'should add expires header if timestamp is supplied in query params' do
         controller.stub :send_data => nil
@@ -28,50 +29,50 @@ describe MediaController, :type => :controller do
     end
 
     describe 'response' do
-      it "should return current child's photo" do
+      it "should return current photo" do
         given_a(model).
                 with_id('1').
                 with_photo(uploadable_photo, 'current')
 
-        get :show_photo, :child_id => '1'
+        get :show_photo, @model_id => '1'
         expect(response).to redirect_to(:photo_id => 'current', :ts => Date.today)
       end
 
-      it "should return requested child's photo" do
+      it "should return requested photo" do
         given_a(model).
                 with_id('1')
         with_photo(uploadable_photo, 'current').
         with_photo(uploadable_photo_jeff, 'other', false)
 
-        get :show_photo, :child_id => '1', :photo_id => 'other'
+        get :show_photo, @model_id => '1', :photo_id => 'other'
         expect(response).to represent_inline_attachment(uploadable_photo_jeff)
       end
 
-      it "should return current child's photo resized to a particular size" do
+      it "should return current photo resized to a particular size" do
         given_a(model).
                 with_id('1').
                 with_photo(uploadable_photo, 'current')
 
-        get :show_resized_photo, :child_id => '1', :size => 300
+        get :show_resized_photo, @model_id => '1', :size => 300
         expect(response).to redirect_to(:photo_id => 'current', :ts => Date.today)
       end
 
-      it "should return current child's photo resized to a particular size" do
+      it "should return current photo resized to a particular size" do
         given_a(model).
                 with_id('1').
                 with_photo(uploadable_photo, 'current')
 
-        get :show_resized_photo, :child_id => '1', :photo_id => 'current', :size => 300
+        get :show_resized_photo, @model_id => '1', :photo_id => 'current', :size => 300
         expect(to_image(response.body)[:width]).to eq(300)
       end
 
-      it "should return requested child's thumbnail" do
+      it "should return requested thumbnail" do
         given_a(model).
                 with_id('1')
         with_photo(uploadable_photo_jeff).
                 with_photo(uploadable_photo, 'other', false)
 
-        get :show_thumbnail, :child_id => '1', :photo_id => 'other'
+        get :show_thumbnail, @model_id => '1', :photo_id => 'other'
 
         thumbnail = to_thumbnail(160, uploadable_photo.original_filename)
         expect(response).to represent_inline_attachment(thumbnail)
@@ -82,7 +83,7 @@ describe MediaController, :type => :controller do
                 with_id('1').
                 with_no_photos
 
-        get :show_photo, :child_id => '1'
+        get :show_photo, @model_id => '1'
         expect(response).to redirect_to(:photo_id => '_missing_')
       end
 
@@ -91,7 +92,7 @@ describe MediaController, :type => :controller do
                 with_id('1').
                 with_no_photos
 
-        get :show_photo, :child_id => '1', :photo_id => '_missing_'
+        get :show_photo, @model_id => '1', :photo_id => '_missing_'
         expect(response).to represent_inline_attachment(no_photo_clip)
       end
 
@@ -103,38 +104,45 @@ describe MediaController, :type => :controller do
               with(:current_photo_key => 'test').
               with(:last_updated_at => 'test')
 
-        get :show_thumbnail, :child_id => '1'
+        get :show_thumbnail, @model_id => '1'
         expect(response).to redirect_to(:photo_id => 'test', :ts => 'test')
       end
     end
 
     describe 'download audio' do
-      it 'should return an amr audio file associated with a child' do
+      it 'should return an amr audio file' do
         given_a(model).
                 with_id('1').
-                with_unique_identifier('child123').
+                with_unique_identifier("#{model}123").
                 with_audio(uploadable_audio_amr)
 
-        get :download_audio, :child_id => '1'
-        expect(response).to represent_attachment(uploadable_audio_amr, 'audio_child123.amr')
+        get :download_audio, @model_id => '1'
+        expect(response).to represent_attachment(uploadable_audio_amr, "audio_#{model}123.amr")
       end
-      
-      it 'should return an mp3 audio file associated with a child' do
+
+      it 'should return an mp3 audio file' do
         given_a(model).
                 with_id('1').
-                with_unique_identifier('child123').
+                with_unique_identifier("#{model}123").
                 with_audio(uploadable_audio_mp3)
 
-        get :download_audio, :child_id => '1'
-        expect(response).to represent_attachment(uploadable_audio_mp3, 'audio_child123.mp3')
+        get :download_audio, @model_id => '1'
+        expect(response).to represent_attachment(uploadable_audio_mp3, "audio_#{model}123.mp3")
       end
     end
   end
 
   describe 'children photos' do
-    let (:model) {'child'}
-    let (:model_class) {'Child'}
-    it_behaves_like 'photos'
+    it_behaves_like 'photos' do 
+      let (:model) {'child'}
+      let (:model_class) {'Child'}
+    end
   end
   
+  describe 'enquiries photos' do
+    it_behaves_like 'photos' do
+      let (:model) {'enquiry'}
+      let (:model_class) {'Enquiry'}
+    end
+  end
 end
