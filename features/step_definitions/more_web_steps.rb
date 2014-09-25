8800c1bc00e04504b70e5bbce9537d1af68ff3c2
andreweskeclarke <andreweskeclarke@gmail.com>
2014-09-25 09:19:37 +0300

    rapidftr/tracker#174 @austiine04 @andreweskeclarke Fixing cucumber audio tag tests, since we removed the audio tag

--- features/step_definitions/more_web_steps.rb
+++ features/step_definitions/more_web_steps.rb
@@ -114,11 +114,13 @@ Then /^I should see an audio element that can play the audio file named "([^"]*)
   url = current_url.gsub '/edit', ''
   url = url.gsub 'children', 'child'
   url = url.gsub 'enquiries', 'enquiry'
-  expect(page.body).to have_selector("//audio/source[@src='#{url}/audio']")
+  expect(page.body).to have_selector(:css, ".audio a[href='#{url}/audio']")
+  expect(page.body).to have_selector(:css, '.audio_player')
+  expect(page.body).to have_selector(:css, '.jp-audio')
 end
 
 Then /^I should not see an audio tag$/ do
-  expect(page.body).not_to have_selector('//audio')
+  expect(page.body).not_to have_selector(:css, '.audio_player')
 end
 
 When /^I visit the "([^"]*)" tab$/ do |name_of_tab|
