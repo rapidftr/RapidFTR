fc882deb6e180cb0b5354739a84ac1127d909b07
Chua Jie Sheng <jschua@thoughtworks.com>
2014-11-20 16:50:24 +0800

    #127 | Jie Sheng | Replace steps definition

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,10 @@
+When /^the user "([^"]*)" checkbox is marked as "([^"]*)"$/ do |username, _status|
+  select('All', :from => 'filter')
+  disabled_checkbox = find(:css, "#user-row-#{username} td.user-status input")
+  disabled_checkbox.click
+  click_button('Yes')
+end
+
+When /^I expire my session$/ do
+  Clock.stub(:now).and_return(21.minutes.from_now)
+end
