3666d391d38a7cdbbf83d9a99b28fd3188b3f20c
timothyakampa <timothyakampa@gmail.com>
2015-09-04 20:12:30 +0300

    Fix failing test on CI

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -343,6 +343,6 @@ When /^I click OK in the browser popup$/ do
   page.driver.browser.switch_to.alert.accept
 end
 
-When(/^I follow "(.*?)" for enquiry records$/) do
+When(/^I follow "([^"]*)" for enquiry records$/) do |_|
   find(:xpath, "//span[@class='export']").click
 end
